ba9ed04a5e39f0ed4870eefbfc18128e4abe2b8d
Taj Loodu <tajloodu@gmail.com>
2022-10-30 17:20:47 -0400

    Fixed spacing issue for mobile

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,5 +1,5 @@
 import './Hero.css';
-import { Box, Button, useMediaQuery, useTheme } from '@mui/material';
+import { Box, Button, useMediaQuery, useTheme, darken } from '@mui/material';
 import 'boxicons';
 import HeroImg from '../../assets/svgs/parliament-hero-asset.svg';
 import BannerLogo from '../../assets/Logos/hthlogo_banner_ver.svg';
@@ -8,17 +8,18 @@ function Hero() {
 
   const theme = useTheme();
   const matchesSM = useMediaQuery(theme.breakpoints.down("sm"))
+  const matchesMD = useMediaQuery(theme.breakpoints.down("md"))
 
   return (
     <div className="Hero">
       <Box sx={{
         width: '100%',
-        marginTop: '13%',
+        marginTop: matchesMD ? '35%' : '13%',
         transition: '1s'
       }}>
         <Box sx={{
           marginLeft: '5%',
-          width: matchesSM ? '80%' : '50%',
+          width: matchesSM ? '85%' : '50%',
           position: matchesSM ? 'relative' : 'absolute',
           transition: '1s'
         }}>
@@ -32,17 +33,20 @@ function Hero() {
                 backgroundColor:'#3B4779', 
                 borderRadius:'100px', 
                 color:'white', 
-                mt:'2vw', 
+                mt:'2vw',
                 textTransform:'none',
                 width:'25%',
-                fontSize:'1.10vw'
+                fontSize:'1.10vw',
+                justifyContent:'space-between',
+                '&:hover': {
+                  backgroundColor: darken('#3B4779', 0.3)}
               }}>
-                  Learn More →
+                  <p class='ButtonText'>Learn More</p>
               </Button>
             </Box>
         </Box>
         <Box sx={{
-          mt: matchesSM ? '10%' : '0%',
+          mt: matchesSM ? '40%' : '0%',
           transition: '1s'
         }}>
           <img class="HeroImg" src={HeroImg}></img>
