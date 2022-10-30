c71a59e3eacc922df3c0ec5a347ee07321ee252b
Arman <arman.dogru@gmail.com>
2022-10-30 16:28:03 -0400

    About us feature

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,30 @@
-import './About.css';
+import React, { Component } from "react";
+import "./About.css";
 
-function About() {
-  return (
-    <div className="About">
-        About
-    </div>
-  );
+class About extends Component {
+  render() {
+    return (
+      <div className="About">
+          <h3>About Us</h3>
+        <div class="vl"><div class="about-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Nam malesuada dui ut lacus commodo, eget pharetra dui 
+            sollicitudin. Cras luctus dui ut posuere imperdiet. 
+            Nullam accumsan lectus ut blandit dictum. In blandit 
+            neque justo, a sagittis tellus aliquam in. Aliquam 
+            lacinia facilisis bibendum. Curabitur eleifend ultrices 
+            diam, ac molestie odio pretium ut. Etiam in turpis vitae 
+            lorem vestibulum blandit sed id nulla. Praesent bibendum 
+            enim sit amet felis luctus, a tincidunt magna fringilla. 
+            Sed interdum, lorem non iaculis interdum, nunc nunc blandit 
+            lacus, sit amet pretium urna nisl non risus. Maecenas in 
+            magna eu tortor convallis efficitur eu sit amet libero. 
+            Duis magna sapien, dapibus ac tempus iaculis, bibendum nec 
+            nulla. Mauris cursus mattis risus, eu blandit massa 
+            consequat aliquam.</div>
+            </div>
+      </div>
+    );
+  }
 }
 
 export default About;
