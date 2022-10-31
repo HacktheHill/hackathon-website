0d625197d0a69aab18973ad9f33803153d24d3ed
Victor Sandru <vsandru4@gmail.com>
2022-10-31 00:29:06 -0400

    worked on faq

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -1,11 +1,29 @@
-import './FAQ.css';
+import styles from "./FAQ.module.css";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Typography,
+} from "@mui/material";
+import "boxicons";
 
-function FAQ() {
+export default function FAQ() {
+  const quesAns = [
+    { q: "question 1!", a: "answer to question 1!" },
+    { q: "question 1!", a: "answer to q2!" },
+  ];
+
   return (
-    <div className="FAQ">
-        FAQ
+    <div className={styles.faqContainer}>
+      <div className={styles.faqHeader}>Frequently Asked Questions</div>
+      {quesAns.map((i) => (
+        <Accordion className={styles.questionContainer}>
+          <AccordionSummary className={styles.question}>{i.q}</AccordionSummary>
+          <AccordionDetails className={styles.answer}>
+            <Typography align={"left"}>{i.a}</Typography>
+          </AccordionDetails>
+        </Accordion>
+      ))}
     </div>
   );
 }
-
-export default FAQ;
