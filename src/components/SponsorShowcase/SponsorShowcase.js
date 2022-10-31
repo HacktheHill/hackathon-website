58cfdbaedce9acc6d0639960d4375b35a4d591c4
Arman <arman.dogru@gmail.com>
2022-10-31 18:48:53 -0400

    Added logo and edited sponsors section

--- src/components/SponsorShowcase/SponsorShowcase.js
+++ src/components/SponsorShowcase/SponsorShowcase.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Logo from './laptop_asset.svg';
 import "./SponsorShowcase.css";
 
 class SponsorShowcase extends Component {
@@ -6,22 +7,12 @@ class SponsorShowcase extends Component {
     return (
       <div className="Sponsors">
           <h3>Sponsors</h3>
-        <div class="vl"><div class="sponsors-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Nam malesuada dui ut lacus commodo, eget pharetra dui 
-            sollicitudin. Cras luctus dui ut posuere imperdiet. 
-            Nullam accumsan lectus ut blandit dictum. In blandit 
-            neque justo, a sagittis tellus aliquam in. Aliquam 
-            lacinia facilisis bibendum. Curabitur eleifend ultrices 
-            diam, ac molestie odio pretium ut. Etiam in turpis vitae 
-            lorem vestibulum blandit sed id nulla. Praesent bibendum 
-            enim sit amet felis luctus, a tincidunt magna fringilla. 
-            Sed interdum, lorem non iaculis interdum, nunc nunc blandit 
-            lacus, sit amet pretium urna nisl non risus. Maecenas in 
-            magna eu tortor convallis efficitur eu sit amet libero. 
-            Duis magna sapien, dapibus ac tempus iaculis, bibendum nec 
-            nulla. Mauris cursus mattis risus, eu blandit massa 
-            consequat aliquam.</div>
-            </div>
+        <div class="vl"><div class="sponsors-body">Hack the Hill is made possible by our generous sponsors. 
+        Interested in supporting one of Ottawa’s largest hackathons?</div>
+        </div>
+          <div class="logo">
+              <img src={Logo} alt="React Logo" />
+          </div>
       </div>
     );
   }
