5107d874d08a48d44845bcfe4ebfe6f7fec9e34d
Taj Loodu <tajloodu@gmail.com>
2022-10-31 10:26:38 -0400

    Fixed Mobile screen formatting issues and added sample descriptions for events

--- src/components/Schedule/Schedule.js
+++ src/components/Schedule/Schedule.js
@@ -1,58 +1,132 @@
 import Paper from "@mui/material/Paper";
 import Stack from "@mui/material/Stack";
 import Box from "@mui/material/Box";
-import { styled } from "@mui/material/styles";
+import { Card } from "@mui/material"
+import { useTheme } from "@mui/material";
+import { useMediaQuery } from "@mui/material";
 import React from "react";
 import './Schedule.css';
-
-const Item = styled('div')(({ theme }) => ({
-
-}));
+import { styled } from "@mui/material";
 
 function Schedule() {
+
+  const theme = useTheme();
+  const matchesSM = useMediaQuery(theme.breakpoints.down("sm"))
+  const matchesMD = useMediaQuery(theme.breakpoints.down("md"))
+  
   return (
     <div id="Schedule" className="Schedule">
       <h2 className="scheduleTitle">Schedule</h2>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-        <Box sx={{ width: "30%", margin: "auto" }}>
-          <Stack spacing={4} id="eventStack" class="eventStack">
+        <Box sx={{ 
+          width: matchesMD ? '70%' : "60%", 
+          margin:'auto',
+          mt: matchesSM ? '15%' : 'auto'
+        }}>
+          <Stack spacing={10} id="eventStack" class="eventStack">
 
-            <Stack direction = "row" spacing={2}>
-              <div className ="event">
+            <Stack direction = {{xs: "column", md: "row"}} spacing={matchesMD ? 5 : 10} sx={{
+              width:'100%',
+
+            }}>
+              <div class='Center'>
+              <Card variant='outlined' elevation={4} sx={{
+                minWidth: '150px',
+                maxWidth:'150px',
+                backgroundColor:'#3b477a',
+                border:'3px solid white',
+                borderRadius:'10px',
+                maxHeight:'140px',
+                mt: matchesMD ? '10px' : '0px',
+                boxShadow: 'rgba(0, 0, 0, 0.15) 0px 15px 25px, rgba(0, 0, 0, 0.05) 0px 5px 10px',
+                transition: 'transform 330ms ease-in-out',
+                '&:hover': {
+                  transform:'scale(1.05)',
+                  transition: 'transform 330ms ease-in-out'
+                }
+              }}>
                 <div className ="eventMonth">November</div>
                 <div className ="eventDay">16</div>
+              </Card>
               </div>
-              <hr></hr><hr></hr><hr></hr><hr></hr>
-              <div>
-                <br></br>
-                <div className ="eventTitle">Wicked Web Dev</div>
-              </div>
+              <Stack >
+                <div className ="eventTitle">Resume Roast</div>
+                <p align='left'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                 Duis nisi nisi, sagittis id massa a, malesuada interdum mi. Suspendisse
+                  in cursus quam. Aliquam condimentum luctus nisl, a scelerisque tellus vulputate nec. 
+                  Phasellus sit amet iaculis nibh. Nulla sit amet lacus vestibulum, elementum dolor vitae, 
+                  mollis mi. Nam vitae ultrices nibh, id varius dolor. </p>
+              </Stack>
             </Stack>
 
-            <Stack direction = "row" spacing={2}>
-              <div className ="event">
+            <Stack direction = {{xs: "column", md: "row"}} spacing={matchesMD ? 5 : 10} sx={{
+              width:'100%',
+
+            }}>
+              <div class='Center'>
+              <Card variant='outlined' sx={{
+                minWidth: '150px',
+                maxWidth:'150px',
+                backgroundColor:'#3b477a',
+                border:'3px solid white',
+                borderRadius:'10px',
+                maxHeight:'140px',
+                mt: matchesMD ? '10px' : '0px',
+                boxShadow: 'rgba(0, 0, 0, 0.15) 0px 15px 25px, rgba(0, 0, 0, 0.05) 0px 5px 10px',
+                transition: 'transform 330ms ease-in-out',
+                '&:hover': {
+                  transform:'scale(1.05)',
+                  transition: 'transform 330ms ease-in-out'
+                }
+              }}>
                 <div className ="eventMonth">November</div>
                 <div className ="eventDay">29</div>
+              </Card>
               </div>
-              <hr></hr><hr></hr><hr></hr><hr></hr>
               <div>
-                <br></br>
-                <div className ="eventTitle">Resume Roast</div>
-              </div>
+                <div className ="eventTitle">Coffee Code & Cram</div>
+                <p align='left'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                 Duis nisi nisi, sagittis id massa a, malesuada interdum mi. Suspendisse
+                  in cursus quam. Aliquam condimentum luctus nisl, a scelerisque tellus vulputate nec. 
+                  Phasellus sit amet iaculis nibh. Nulla sit amet lacus vestibulum, elementum dolor vitae, 
+                  mollis mi. Nam vitae ultrices nibh, id varius dolor. </p>              
+                </div>
             </Stack>
 
-            <Stack direction = "row" spacing={2}>
-              <div className ="event">
-                <div className ="eventMonth">January</div>
-                <div className ="eventDay">11</div>
-              </div>
-              <hr></hr><hr></hr><hr></hr><hr></hr>
-              <div>
-                <br></br>
-                <div className ="eventTitle">Coffee, Code and Cram</div>
-              </div>
-            </Stack>
+            <Stack direction = {{xs: "column", md: "row"}} spacing={matchesMD ? 5 : 10} sx={{
+              width:'100%',
 
+            }}>
+              <div class='Center'>
+              <Card variant='outlined' sx={{
+                minWidth: '150px',
+                maxWidth:'150px',
+                backgroundColor:'#3b477a',
+                border:'3px solid white',
+                borderRadius:'10px',
+                maxHeight:'140px',
+                mt: matchesMD ? '10px' : '0px',
+                boxShadow: 'rgba(0, 0, 0, 0.15) 0px 15px 25px, rgba(0, 0, 0, 0.05) 0px 5px 10px',
+                transition: 'transform 330ms ease-in-out',
+                '&:hover': {
+                  transform:'scale(1.05)',
+                  transition: 'transform 330ms ease-in-out'
+                }
+              }}>
+                  
+                  <div className ="eventMonth">January</div>
+                  <div className ="eventDay">11</div>
+                </Card>
+              </div>
+              <div>
+                <div className ="eventTitle">Wicked Web Dev</div>
+                <p align='left'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                 Duis nisi nisi, sagittis id massa a, malesuada interdum mi. Suspendisse
+                  in cursus quam. Aliquam condimentum luctus nisl, a scelerisque tellus vulputate nec. 
+                  Phasellus sit amet iaculis nibh. Nulla sit amet lacus vestibulum, elementum dolor vitae, 
+                  mollis mi. Nam vitae ultrices nibh, id varius dolor. </p>              
+                </div>
+            </Stack>
           </Stack>
         </Box>
     </div>
