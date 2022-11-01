e3c086a29677c5b9a755682691315ab61f1c0046
Taj Loodu <tajloodu@gmail.com>
2022-11-01 18:03:24 -0400

    Added buttons for linking to Eventbrite and fixed mobile view of design assets

--- src/components/Schedule/Schedule.js
+++ src/components/Schedule/Schedule.js
@@ -1,126 +1,195 @@
-import Stack from "@mui/material/Stack";
-import Box from "@mui/material/Box";
-import { Card } from "@mui/material"
-import { useTheme } from "@mui/material";
-import { useMediaQuery } from "@mui/material";
 import React from "react";
 import './Schedule.css';
-import SnowFlake from '../../assets/svgs/snowflake_1.svg'
+import { Card, Stack, Box, useTheme, useMediaQuery, Button, darken } from "@mui/material";
 
 function Schedule() {
-
   const theme = useTheme();
-  const matchesSM = useMediaQuery(theme.breakpoints.down("sm"))
-  const matchesMD = useMediaQuery(theme.breakpoints.down("md"))
+  const matchesSM = useMediaQuery(theme.breakpoints.down("sm"));
+  const matchesMD = useMediaQuery(theme.breakpoints.down("md"));
+  const matchesLG = useMediaQuery(theme.breakpoints.down("lg"));
 
   const styles = {
     card: {
       minWidth: '150px',
-        maxWidth:'150px',
-        backgroundColor:'#3b477a',
-        border:'3px solid white',
-        borderRadius:'10px',
-        maxHeight:'140px',
-        mt: matchesMD ? '10px' : '0px',
-        boxShadow: 'rgba(0, 0, 0, 0.15) 0px 15px 25px, rgba(0, 0, 0, 0.05) 0px 5px 10px',
-        transition: 'transform 330ms ease-in-out',
-        '&:hover': {
-          transform:'scale(1.05)',
-          transition: 'transform 330ms ease-in-out'
-        }
+      maxWidth:'150px',
+      backgroundColor:'#3b477a',
+      border:'3px solid white',
+      borderRadius:'10px',
+      maxHeight:'140px',
+      mt: matchesMD ? '10px' : '0px',
+      boxShadow: 'rgba(0, 0, 0, 0.15) 0px 15px 25px, rgba(0, 0, 0, 0.05) 0px 5px 10px',
+      transition: 'transform 330ms ease-in-out',
+      '&:hover': {
+        transform:'scale(1.05)',
+        transition: 'transform 330ms ease-in-out'
       }
-    }
+    },
+  }
   
   return (
     <div id="Schedule" className="Schedule">
       <h1 className="scheduleTitle">Schedule</h1>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-        <Box sx={{ 
-          width: matchesMD ? '70%' : "60%", 
-          margin:'auto',
-          mt: matchesSM ? '15%' : 'auto'
-        }}>
-          <Stack spacing={10} id="eventStack" class="eventStack">
-
-            <Stack direction = {{xs: "column", md: "row"}} spacing={matchesMD ? 5 : 10} sx={{
-              width:'100%',
-
-            }}>
-              <div class='Center'>
-                <Card variant='outlined' elevation={4} style={styles.card} sx={{
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+      <Box sx={{ 
+        width: matchesMD ? '75%' : "70%", 
+        margin:'auto',
+        mt: matchesSM ? '15%' : 'auto'
+        }}
+      >
+        <Stack spacing={10} id="eventStack" class="eventStack">
+          <Stack direction = {{xs: "column", md: "row"}} spacing={matchesMD ? 5 : 10} sx={{ width:'100%'}}>
+            <div class='Center'>
+              <Card 
+                variant='outlined' 
+                elevation={4} 
+                style={styles.card} 
+                sx={{
                   '&:hover': {
                     transform:'scale(1.05)',
                     transition: 'transform 330ms ease-in-out'
                   }
-                }}>
-                  <Stack direction={'column'}>
-                    <div className ="eventMonth">November</div>
-                    <div className ="eventDay">16</div>
-                  </Stack>
-                </Card>
+                }}
+              >
+                <Stack direction={'column'}>
+                  <div className ="eventMonth">November</div>
+                  <div className ="eventDay">16</div>
+                </Stack>       
+              </Card>
+            </div>
+            <Stack sx={{ width:'100%' }}>
+              <div className ="eventTitle">
+                Resume Roast    
+                <Button sx={{
+                  background:'linear-gradient(90deg, #ABEFFB, transparent) #E9D9F2', 
+                  borderRadius:'100px', 
+                  color:'#3B4779', 
+                  mt: matchesMD ? '3vw' : '0vw',
+                  mb: matchesMD ? '3vw' : '0vw',
+                  p:'5px',
+                  textTransform:'none',
+                  minWidth:'16%',
+                  width: matchesSM ? '50%' : matchesLG ? '25%' : '0%',
+                  transition: '1s',
+                  boxShadow: 'rgba(0, 0, 0, 0.15) 0px 15px 25px, rgba(0, 0, 0, 0.05) 0px 5px 10px',
+                  '&:hover': {
+                    backgroundColor: '#f094bc',
+                  }
+                  }}
+                >
+                  <p class='ButtonText'>Sign up <b>→</b></p>
+                </Button>
+                  
               </div>
-              <Stack >
-                <div className ="eventTitle">Resume Roast</div>
-                <div className ="subTitle">7 p.m - 9 p.m @ STM 117</div>
-                <p className="eventDesc" align='left'>Need resume advice on the spot? Hang out with some developers 
+              <div className ="subTitle">7 p.m. - 9 p.m. @ STM 117</div>
+              <p className="eventDesc" align='left'>
+                Need resume advice on the spot? Hang out with some developers 
                 from Ciena and Microsoft as they review and evaluate resumes submitted by students, 
-                all while filling yourself up with some pizza fresh from fresh pizza. </p>
-              </Stack>
+                all while filling yourself up with some pizza fresh from fresh pizza. 
+              </p>
             </Stack>
+          </Stack>
 
-            <Stack direction = {{xs: "column", md: "row"}} spacing={matchesMD ? 5 : 10} sx={{
-              width:'100%',
-
-            }}>
-              <div class='Center'>
-              <Card variant='outlined' elevation={4} style={styles.card} sx={{
+          <Stack direction = {{xs: "column", md: "row"}} spacing={matchesMD ? 5 : 10} sx={{ width:'100%' }}>
+            <div class='Center'>
+              <Card 
+                variant='outlined' 
+                elevation={4} 
+                style={styles.card} 
+                sx={{
                   '&:hover': {
                     transform:'scale(1.05)',
                     transition: 'transform 330ms ease-in-out'
                   }
-                }}>
+                }}
+              >
                 <Stack direction={'column'}>
                   <div className ="eventMonth">November</div>
                   <div className ="eventDay">29</div>
-                </Stack>
+                </Stack>       
               </Card>
+            </div>
+            <Stack sx={{ width:'100%' }}>
+              <div className ="eventTitle">
+                Coffee, Code & Cram
+                <Button disabled sx={{
+                  background:'linear-gradient(90deg, #ABEFFB, transparent) #E9D9F2', 
+                  borderRadius:'100px', 
+                  color:'#3B4779', 
+                  mt: matchesMD ? '3vw' : '0vw',
+                  mb: matchesMD ? '3vw' : '0vw',
+                  p:'7px',
+                  textTransform:'none',
+                  minWidth:'16%',
+                  width: matchesSM ? '50%' : matchesLG ? '25%' : '0%',
+                  transition: '1s',
+                  '&:hover': {
+                    backgroundColor: '#f094bc',
+                  }
+                  }}
+                >
+                  <p class='ButtonText'>Sign up <b>→</b></p>
+                </Button>    
               </div>
-              <div>
-                <div className ="eventTitle">Coffee Code & Cram</div>
-                <div className ="subTitle">7 p.m - 9 p.m @ STM 117</div>
-                <p className="eventDesc" align='left'>Unwind and take a break from school.
-                 Come along for an evening of socializing and coding with 
-                 coffee, tea, and snacks. </p>              
-                </div>
+              <div className ="subTitle">7 p.m. - 9 p.m. @ STM 117</div>
+              <p className="eventDesc" align='left'>
+                Unwind and take a break from school.
+                Come along for an evening of socializing and coding with 
+                coffee, tea, and snacks.
+              </p>              
             </Stack>
-
-            <Stack direction = {{xs: "column", md: "row"}} spacing={matchesMD ? 5 : 10} sx={{
-              width:'100%',
-
-            }}>
-              <div class='Center'>
-              <Card variant='outlined' elevation={4} style={styles.card} sx={{
+          </Stack>
+          
+          <Stack direction = {{xs: "column", md: "row"}} spacing={matchesMD ? 5 : 10} sx={{ width:'100%' }}>
+            <div class='Center'>
+              <Card 
+                variant='outlined' 
+                elevation={4} 
+                style={styles.card} 
+                sx={{
                   '&:hover': {
                     transform:'scale(1.05)',
                     transition: 'transform 330ms ease-in-out'
-                  }
-                }}>
+                }
+                }}
+              >
                 <Stack direction={'column'}>
                   <div className ="eventMonth">January</div>
                   <div className ="eventDay">11</div>
-                </Stack>
-                </Card>
+                </Stack>       
+              </Card>
+            </div>
+            <Stack sx={{ width:'100%' }}>
+              <div className ="eventTitle">
+                Wicked Web Dev
+                <Button disabled sx={{
+                  background:'linear-gradient(90deg, #ABEFFB, transparent) #E9D9F2', 
+                  borderRadius:'100px', 
+                  color:'#3B4779', 
+                  mt: matchesMD ? '3vw' : '0vw',
+                  mb: matchesMD ? '3vw' : '0vw',
+                  p:'7px',
+                  textTransform:'none',
+                  minWidth:'16%',
+                  width: matchesSM ? '50%' : matchesLG ? '25%' : '0%',
+                  transition: '1s',
+                  '&:hover': {
+                    backgroundColor: '#f094bc',
+                  }
+                  }}
+                >
+                  <p class='ButtonText'>Sign up <b>→</b></p>
+                </Button> 
               </div>
-              <div>
-                <div className ="eventTitle">Wicked Web Dev</div>
-                <div className ="subTitle">6:30 p.m - 9:30 p.m @ STM 117</div>
-                <p className="eventDesc" align='left'>Explore the wonders of front-end development! 
-                Learn the basics through workshops and participate in a one-hour challenge to build a 
-                website according to provided guidelines to win some sweet prizes.</p>              
-                </div>
+              <div className ="subTitle">6:30 p.m. - 9:30 p.m. @ STM 117</div>
+              <p className="eventDesc" align='left'>
+                Explore the wonders of front-end development! 
+                Learn the basics through workshops and participate in a one-hour challenge to build 
+                a website according to provided guidelines to win some sweet prizes.
+              </p>              
             </Stack>
           </Stack>
-        </Box>
+        </Stack>
+      </Box>
     </div>
   );
 }
