bdc356f5cdccebb86f8dd6c06a3a2eba2ae61ccf
Taj Loodu <tajloodu@gmail.com>
2022-11-01 00:16:41 -0400

    Styled Schedule section and added fonts

--- src/components/Schedule/Schedule.js
+++ src/components/Schedule/Schedule.js
@@ -16,7 +16,7 @@ function Schedule() {
   
   return (
     <div id="Schedule" className="Schedule">
-      <h2 className="scheduleTitle">Schedule</h2>
+      <h1 className="scheduleTitle">Schedule</h1>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         <Box sx={{ 
           width: matchesMD ? '70%' : "60%", 
@@ -30,32 +30,32 @@ function Schedule() {
 
             }}>
               <div class='Center'>
-              <Card variant='outlined' elevation={4} sx={{
-                minWidth: '150px',
-                maxWidth:'150px',
-                backgroundColor:'#3b477a',
-                border:'3px solid white',
-                borderRadius:'10px',
-                maxHeight:'140px',
-                mt: matchesMD ? '10px' : '0px',
-                boxShadow: 'rgba(0, 0, 0, 0.15) 0px 15px 25px, rgba(0, 0, 0, 0.05) 0px 5px 10px',
-                transition: 'transform 330ms ease-in-out',
-                '&:hover': {
-                  transform:'scale(1.05)',
-                  transition: 'transform 330ms ease-in-out'
-                }
-              }}>
-                <div className ="eventMonth">November</div>
-                <div className ="eventDay">16</div>
-              </Card>
+                <Card variant='outlined' elevation={4} sx={{
+                  minWidth: '150px',
+                  maxWidth:'150px',
+                  backgroundColor:'#3b477a',
+                  border:'3px solid white',
+                  borderRadius:'10px',
+                  maxHeight:'140px',
+                  mt: matchesMD ? '10px' : '0px',
+                  boxShadow: 'rgba(0, 0, 0, 0.15) 0px 15px 25px, rgba(0, 0, 0, 0.05) 0px 5px 10px',
+                  transition: 'transform 330ms ease-in-out',
+                  '&:hover': {
+                    transform:'scale(1.05)',
+                    transition: 'transform 330ms ease-in-out'
+                  }
+                }}>
+                  <Stack direction={'column'}>
+                    <div className ="eventMonth">November</div>
+                    <div className ="eventDay">16</div>
+                  </Stack>
+                </Card>
               </div>
               <Stack >
                 <div className ="eventTitle">Resume Roast</div>
-                <p align='left'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                 Duis nisi nisi, sagittis id massa a, malesuada interdum mi. Suspendisse
-                  in cursus quam. Aliquam condimentum luctus nisl, a scelerisque tellus vulputate nec. 
-                  Phasellus sit amet iaculis nibh. Nulla sit amet lacus vestibulum, elementum dolor vitae, 
-                  mollis mi. Nam vitae ultrices nibh, id varius dolor. </p>
+                <p className="eventDesc" align='left'>Need resume advice on the spot? Hang out with some developers 
+                from Ciena and Microsoft as they review and evaluate resumes submitted by students, 
+                all while filling yourself up with some pizza fresh from Domino's Pizza. </p>
               </Stack>
             </Stack>
 
@@ -79,17 +79,17 @@ function Schedule() {
                   transition: 'transform 330ms ease-in-out'
                 }
               }}>
-                <div className ="eventMonth">November</div>
-                <div className ="eventDay">29</div>
+                <Stack direction={'column'}>
+                  <div className ="eventMonth">November</div>
+                  <div className ="eventDay">29</div>
+                </Stack>
               </Card>
               </div>
               <div>
                 <div className ="eventTitle">Coffee Code & Cram</div>
-                <p align='left'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                 Duis nisi nisi, sagittis id massa a, malesuada interdum mi. Suspendisse
-                  in cursus quam. Aliquam condimentum luctus nisl, a scelerisque tellus vulputate nec. 
-                  Phasellus sit amet iaculis nibh. Nulla sit amet lacus vestibulum, elementum dolor vitae, 
-                  mollis mi. Nam vitae ultrices nibh, id varius dolor. </p>              
+                <p className="eventDesc" align='left'>Unwind and take a break from school.
+                 Come along for an evening of socializing and coding with 
+                 coffee, tea, and snacks. </p>              
                 </div>
             </Stack>
 
@@ -113,18 +113,17 @@ function Schedule() {
                   transition: 'transform 330ms ease-in-out'
                 }
               }}>
-                  
+                <Stack direction={'column'}>
                   <div className ="eventMonth">January</div>
                   <div className ="eventDay">11</div>
+                </Stack>
                 </Card>
               </div>
               <div>
                 <div className ="eventTitle">Wicked Web Dev</div>
-                <p align='left'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                 Duis nisi nisi, sagittis id massa a, malesuada interdum mi. Suspendisse
-                  in cursus quam. Aliquam condimentum luctus nisl, a scelerisque tellus vulputate nec. 
-                  Phasellus sit amet iaculis nibh. Nulla sit amet lacus vestibulum, elementum dolor vitae, 
-                  mollis mi. Nam vitae ultrices nibh, id varius dolor. </p>              
+                <p className="eventDesc" align='left'>Explore the wonders of front-end development! 
+                Learn the basics through workshops and participate in a one-hour challenge to build a 
+                website according to provided guidelines to win some sweet prizes.</p>              
                 </div>
             </Stack>
           </Stack>
