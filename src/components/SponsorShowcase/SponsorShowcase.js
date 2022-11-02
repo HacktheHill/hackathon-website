10f03815e5c71fddec0c6f9021a48874f43c1645
Taj Loodu <tajloodu@gmail.com>
2022-11-02 01:25:45 -0400

    Structured and styled Sponsor section

--- src/components/SponsorShowcase/SponsorShowcase.js
+++ src/components/SponsorShowcase/SponsorShowcase.js
@@ -8,27 +8,29 @@ function SponsorShowcase() {
   const theme = useTheme();
   const matchesSM = useMediaQuery(theme.breakpoints.down("sm"))
   const matchesMD = useMediaQuery(theme.breakpoints.down("md"))
+  const matchesLG = useMediaQuery(theme.breakpoints.down("lg"));
+
 
     return (
       <div className="Sponsors">
         <h1>Sponsors</h1>
         <div className="out">
-          <div class="borderW">
             <div class="sponsors-body">
-              Hack the Hill is made possible by our generous sponsors.
-              Interested in supporting one of Ottawa's largest hackathons?
-            </div>
-            <Button href={`mailto:sponsorship@hackthehill.com`} sx={{
+            <p class="text">
+                Hack the Hill is made possible by our generous sponsors.
+                Interested in supporting one of Ottawa's largest hackathons?
+              </p>
+              <Button href={`mailto:sponsorship@hackthehill.com`} sx={{
               backgroundColor: '#5C71AD',
               borderRadius: '100px',
               color: 'white',
               mt: matchesSM ? '7vw' : '2vw',
               p: '10px',
               textTransform: 'none',
-              minWidth: '20%',
-              width: matchesSM ? '100%' : '50%',
+              minWidth: '12%',
+              width: matchesSM ? '50%' : matchesLG ? '20%' : '0%',
               transition: '1s',
-              ml: '20px',
+              ml: '2.5%',
               '&:hover': {
                 backgroundColor: darken('#5C71AD', 0.3)
               }
@@ -36,7 +38,6 @@ function SponsorShowcase() {
               <p class='ButtonText'>Get in touch</p>
             </Button>
           </div>
-          <div><img src={Mascot}></img></div>
         </div>
 
       </div>
