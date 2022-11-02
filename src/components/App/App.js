c2620c803576466f13e3cbc3070deea9add162fd
Victor <vsandru4@gmail.com>
2022-11-02 14:32:37 -0400

    Feature/faq (#8)
    * worked on faq
    
    * added css
    
    * Initial styling for Accordian FAQs
    
    * added color when clicked on
    
    * Added fonts
    
    * Spacing of section matches rest of site
    
    * fixed formatting when q is clicked
    
    * changed padding on answer container
    
    Co-authored-by: Taj Loodu <tajloodu@gmail.com>

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,21 @@
-import './App.css';
-import NavBar from '../NavBar/NavBar.js';
-import Sidebar from '../NavBar/Sidebar.js'
-import Hero from '../Hero/Hero.js';
-import About from '../About/About.js';
-import Schedule from '../Schedule/Schedule.js';
-import SponsorEvent from '../SponsorEvent/SponsorEvent.js';
-import SponsorShowcase from '../SponsorShowcase/SponsorShowcase.js';
-import Collaborators from '../Collaborators/Collaborators.js';
-import FAQ from '../FAQ/FAQ.js';
-import Footer from '../Footer/Footer.js';
+import "./App.css";
+import NavBar from "../NavBar/NavBar.js";
+import Sidebar from "../NavBar/Sidebar.js";
+import Hero from "../Hero/Hero.js";
+import About from "../About/About.js";
+import Schedule from "../Schedule/Schedule.js";
+import SponsorEvent from "../SponsorEvent/SponsorEvent.js";
+import SponsorShowcase from "../SponsorShowcase/SponsorShowcase.js";
+import Collaborators from "../Collaborators/Collaborators.js";
+import FAQ from "../FAQ/FAQ.js";
+import Footer from "../Footer/Footer.js";
 
 function App() {
   return (
-    <div className="App" id='outer-container'>
-      <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
-      <div className='page-wrap'>
-        <div class='Gradient'>
+    <div className="App" id="outer-container">
+      <Sidebar pageWrapId={"page-wrap"} outerContainerId={"outer-container"} />
+      <div className="page-wrap">
+        <div class="Gradient">
           <NavBar />
           <Hero />
         </div>
