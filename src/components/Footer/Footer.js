6a170debffaded932458b23ec627493af1940374
Taj Loodu <tajloodu@gmail.com>
2022-11-02 10:52:39 -0400

    Fixed blank space issues on small mobile screens

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,38 +5,7 @@ import FooterLogo from "../../assets/Logos/hthlogo_banner_ver.svg";
 function Footer() {
   return (
     <div className="Footer">
-      <div className="FooterBody">
-        <div className="FooterLogo">
-          <img src={FooterLogo} alt="Logo" />
-        </div>
-        <div className="LogoText">
-          <p>Canada's Capital Hackathon.</p>
-        </div>
-        <div class="FooterContainer">
-          <div className="MediaLinks">
-            <a href="https://www.facebook.com/canadascapitalhackathon" target="_blank">
-              <box-icon color='#3B4779' animation='tada-hover' type="logo" name="facebook-square"/>
-            </a>
-            <a href="https://twitter.com/hackthehiII" target="_blank">
-              <box-icon color='#3B4779' animation='tada-hover' name="twitter" type="logo" />
-            </a>
-            <a href="https://www.instagram.com/hackthehill/" target="_blank">
-              <box-icon color='#3B4779' animation='tada-hover' name="instagram" type="logo" />
-            </a>
-            <a href="https://www.tiktok.com/@hackthehill" target="_blank">
-              <box-icon color='#3B4779' animation='tada-hover' name="tiktok" type="logo" />
-            </a>
-            <a href="https://www.linkedin.com/company/hackthehill/" target="_blank">
-              <box-icon color='#3B4779' animation='tada-hover' name='linkedin' type='logo' ></box-icon>
-            </a>
-            <a href="https://www.medium.com/@hackthehill" target="_blank">
-              <box-icon color='#3B4779' animation='tada-hover' name='medium' type='logo' ></box-icon>
-            </a>
-          </div>
-          <div class="Signature">
-          </div>
-        </div>
-      </div>
+      Footer
     </div>
   );
 }
