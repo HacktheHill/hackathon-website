fa055b361f0969e873b97db77a3f609ec19a94db
Jasmine Kokkat <jazzimkokkat@gmail.com>
2022-11-02 03:57:02 -0400

    fix spacing

--- src/components/Collaborators/Collaborators.js
+++ src/components/Collaborators/Collaborators.js
@@ -1,5 +1,5 @@
 import './Collaborators.css';
-import { Grid } from "@mui/material";
+import { Grid, useMediaQuery, useTheme } from "@mui/material";
 import SESA from '../../assets/Logos/sesa.svg'
 import CSSA from '../../assets/Logos/cssa.svg'
 import IEEE from '../../assets/Logos/ieee.svg'
@@ -7,10 +7,14 @@ import ESS from '../../assets/Logos/ess.svg'
 import WIE from '../../assets/Logos/wie.svg'
 
 function Collaborators() {
+  const theme = useTheme();
+  const matchesSM = useMediaQuery(theme.breakpoints.down("sm"));
+  const matchesMD = useMediaQuery(theme.breakpoints.down("md"));
+  const matchesLG = useMediaQuery(theme.breakpoints.down("lg"));
   return (
     <div className="Collaborators">
        <h1>Collaborators </h1>
-        <Grid container id="Associations" className="Associations">
+        <Grid container  id="Associations" className="Associations">
           <Grid item direction="column" className ="CSSABox">
           <a href="https://www.cssa-aei.ca/"><img className="CSSA" alt="CSSA" src={CSSA}></img></a>
           </Grid>
