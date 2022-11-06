452b4542465f66ce2bdce7de6aec89b04a60ef9f
Arman <arman.dogru@gmail.com>
2022-11-05 22:07:07 -0400

    Finished Sponsors showcase

--- src/components/SponsorShowcase/SponsorShowcase.js
+++ src/components/SponsorShowcase/SponsorShowcase.js
@@ -1,7 +1,10 @@
 import React from "react";
 import "./SponsorShowcase.css";
-import { Button, useMediaQuery, useTheme, darken } from '@mui/material';
-import Mascot from '../../assets/Logos/mascot_nametag_waving.svg';
+import { Button, useMediaQuery, useTheme, darken, Grid } from '@mui/material';
+import LoneHeaven from '../../assets/Logos/placeholder_small.svg';
+import StackOverFlow from '../../assets/Logos/Stack_Overflow.svg';
+import MLH from '../../assets/Logos/Mlh-logo-color.svg';
+import Github from '../../assets/Logos/github.svg';
 
 function SponsorShowcase() {
   
@@ -10,14 +13,13 @@ function SponsorShowcase() {
   const matchesMD = useMediaQuery(theme.breakpoints.down("md"))
   const matchesLG = useMediaQuery(theme.breakpoints.down("lg"));
 
-
     return (
       <div className="Sponsors">
         <h1>Sponsors</h1>
         <div className="out">
-            <div class="sponsors-body">
+          <div class="sponsors-body">
             <p class="text">
-            Hack the Hill is made possible by our generous sponsors. 
+            Hack the Hill is made possible by our generous sponsors.
             Interested in supporting the capital’s hackathon?
               </p>
               <Button href={`mailto:sponsorship@hackthehill.com`} sx={{
@@ -37,9 +39,22 @@ function SponsorShowcase() {
             }}>
               <p class='ButtonText'>Get in touch</p>
             </Button>
-          </div>
+          </div> 
         </div>
-
+            <Grid container id="SponsorsDisplay" className="SponsorsDisplay">
+              <Grid item direction="column" className ="SponsorIconBox">
+                <a href="https://stackoverflow.com/"><img className="SponsorIcon" alt="SponsorIcon" src={StackOverFlow}></img></a>
+              </Grid>
+              <Grid item direction="column" className ="SponsorIconBox">
+                <a href="https://mlh.io/"><img className="SponsorIcon" alt="SponsorIcon" src={MLH}></img></a>
+              </Grid>
+              <Grid item direction="column" className ="SponsorIconBox">
+                <a href="https://github.com/about"><img className="SponsorIcon" alt="SponsorIcon" src={Github}></img></a>
+              </Grid>
+              <Grid item direction="column" className ="SponsorIconBox">
+                <a href="https://www.lonehaven.com/"><img className="SponsorIcon" alt="SponsorIcon" src={LoneHeaven}></img></a>
+              </Grid>
+            </Grid> 
       </div>
     );
   }
