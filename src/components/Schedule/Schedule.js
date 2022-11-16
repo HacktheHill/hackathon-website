3d6187f5d60dfac59ce922c696a9678be92afd2e
Arman <arman.dogru@gmail.com>
2022-11-16 15:29:58 -0500

    Disabled the resume roast button

--- src/components/Schedule/Schedule.js
+++ src/components/Schedule/Schedule.js
@@ -59,7 +59,7 @@ function Schedule() {
             <Stack sx={{ width:'100%' }}>
               <div className ="eventTitle">
                 Resume Roast    
-                <Button target='_blank' href='https://forms.gle/X62Qxsfqt3zFHWd16' sx={{
+                <Button disabled sx={{
                   background:'linear-gradient(90deg, #ABEFFB, transparent) #E9D9F2', 
                   borderRadius:'100px', 
                   color:'#3B4779', 
