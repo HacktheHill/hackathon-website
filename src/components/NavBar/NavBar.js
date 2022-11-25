91a8ac7b94951b7b3e6b2a3ea0fec3c2a9d3b5fb
Alex Govier <alex.d.g@icloud.com>
2022-11-25 08:47:17 -0500

    Added MLH badge to navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,11 +1,12 @@
 import './NavBar.css';
 import { Link } from 'react-scroll';
-
+import MLH from '../../assets/Logos/mlhbadge.svg';
 function Navbar() {
   return (
     <div className="Navbar">
-        <img alt='Logo' src="Logos\hthlogo_icon_ver.svg"></img>
+        <img className='logo' alt='Logo' src="Logos\hthlogo_icon_ver.svg"></img>
         <div className='Items'>
+          <a target="_blank" href="https://mlh.io/seasons/2023/events"><img className='mlh' alt="MLH" src={MLH}></img></a>
           <div className='Item'><Link to="FAQ" spy={true} smooth={true} offset={-30} duration={500}>FAQ</Link></div>
           <div className='Item'><Link to="Collaborators" spy={true} smooth={true} offset={-30} duration={500}>Collaborators</Link></div>
           <div className='Item'><Link to="Schedule" spy={true} smooth={true} offset={0} duration={500}>Schedule</Link></div>
