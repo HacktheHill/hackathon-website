76c2e25feea91cee231c8dee854550a6723cb7d5
Stefan Todorovic <stefantodorovic@gmail.com>
2022-11-25 00:16:50 -0500

    tried to add registeration page

---
+++ src/components/PageRouter/PageRouter.js
@@ -0,0 +1,22 @@
+// import react router dom
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from '../App/App.js';
+import Register from '../Register/Register';
+
+function PageRouter() {
+
+  return (
+    <div className="App" id="outer-container">
+        <Router>
+            <Routes>
+                <Route path="/" element={<App/>}/>
+                <Route path="/register" element={<Register />} />
+            </Routes>
+        </Router>
+        
+
+    </div>
+    );
+}
+
+export default PageRouter;
---
+++ src/components/Register/Register.js
@@ -0,0 +1,13 @@
+
+import "./Register.css"
+import embedFILE from "https://embed.typeform.com/next/embed.js"
+
+const Register = () => {
+    embedFILE();
+    return (
+        <div id="wrapper" data-tf-widget="YWPbYwPy" data-tf-inline-on-mobile data-tf-hide-headers data-tf-medium="snippet">
+        </div>
+    )
+}
+
+export default Register
