f1b30b22fa8a8819cbfbe82565a190b99da707c4
Stefan Todorovic <stefantodorovic@gmail.com>
2022-11-25 02:08:20 -0500

    fixed ui issue and routing

--- src/components/PageRouter/PageRouter.js
+++ src/components/PageRouter/PageRouter.js
@@ -11,6 +11,7 @@ function PageRouter() {
             <Routes>
                 <Route path="/" element={<App/>}/>
                 <Route path="/register" element={<Register />} />
+                <Route path="/*"element={<App />} />
             </Routes>
         </Router>
         
