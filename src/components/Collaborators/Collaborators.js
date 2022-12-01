2cef878e9f9128f03210a6e281618af2b45112cf
Jasmine Kokkat <55165117+jkokkat@users.noreply.github.com>
2022-12-01 14:53:32 -0500

    Added collaborators mountain image (#37)
    * added mountain image for collaborator section
    
    * Fixed mountain and flags asset
    
    * fixed spacing between collaborators and faq section
    
    Co-authored-by: iwang14 <isabellewang009@gmail.com>

--- src/components/Collaborators/Collaborators.js
+++ src/components/Collaborators/Collaborators.js
@@ -1,10 +1,6 @@
 import './Collaborators.css';
 import { Grid, useMediaQuery, useTheme } from "@mui/material";
-import SESA from '../../assets/Logos/sesa.svg'
-import CSSA from '../../assets/Logos/cssa.svg'
-import IEEE from '../../assets/Logos/ieee.svg'
-import ESS from '../../assets/Logos/ess.svg'
-import WIE from '../../assets/Logos/wie.svg'
+import mountain from '../../assets/Logos/mountain_flags.svg'
 
 function Collaborators() {
   const theme = useTheme();
@@ -15,21 +11,7 @@ function Collaborators() {
     <div id="Collaborators" className="Collaborators">
        <h1>Collaborators </h1>
         <Grid container id="Associations" className="Associations">
-        <Grid item direction="column" className ="ESSBox">
-          <a href="https://www.essaeg.ca/"><img className="ESS" alt="ESS" src={ESS}></img></a>
-          </Grid>
-        <Grid item direction="column" className ="IEEEBox">
-          <a href="https://ieeeuottawa.ca/"><img className="IEEE" alt="IEEE" src={IEEE}></img></a>
-          </Grid>
-          <Grid item direction="column" className ="CSSABox">
-          <a href="https://www.cssa-aei.ca/"><img className="CSSA" alt="CSSA" src={CSSA}></img></a>
-          </Grid>
-          <Grid item direction="column" className ="SESABox">
-          <a href="https://www.uottawasesa.ca/"><img className="SESA" alt="SESA" src={SESA}></img></a>
-          </Grid>
-          <Grid item direction="column" className ="WIEBox">
-          <a href="https://wie.ieeeottawa.ca/"><img className="WIE" alt="WIE" src={WIE}></img></a>
-          </Grid>
+          <img className="mountain" alt="mountain" src={mountain}></img>
           </Grid>
     </div>
 
