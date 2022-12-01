dbc8036771e4102cfa90c83b04f37acc2d5ee7f1
Bricktech <bricktech2000@gmail.com>
2022-12-01 15:02:24 -0500

    increased navbar close button size

--- src/components/NavBar/Sidebar.js
+++ src/components/NavBar/Sidebar.js
@@ -22,25 +22,24 @@ export default props => {
       <Link to="Collaborators" spy={true} smooth={true} offset={-30} duration={500} onClick={() => handleCloseMenu()}>Collaborators</Link>
       <Link to="FAQ" spy={true} smooth={true} offset={-30} duration={500} onClick={() => handleCloseMenu()}>FAQ</Link>
       <div className='row'>
-            <a href="https://www.facebook.com/canadascapitalhackathon" target="_blank">
-              <box-icon size='sm' color='white' animation='tada-hover' type="logo" name="facebook-square"/>
-              
-            </a>
-            <a href="https://twitter.com/hackthehiII" target="_blank">
-              <box-icon size='sm' color='white' animation='tada-hover' name="twitter" type="logo" />
-            </a>
-            <a href="https://www.instagram.com/hackthehill/" target="_blank">
-              <box-icon size='sm' color='white' animation='tada-hover' name="instagram" type="logo" />
-            </a>
-            <a href="https://www.tiktok.com/@hackthehill" target="_blank">
-              <box-icon size='sm' color='white' animation='tada-hover' name="tiktok" type="logo" />
-            </a>
-            <a href="https://www.linkedin.com/company/hackthehill/" target="_blank">
-              <box-icon size='sm' color='white' animation='tada-hover' name='linkedin' type='logo' ></box-icon>
-            </a>
-            <a href="https://www.medium.com/@hackthehill" target="_blank">
-              <box-icon size='sm' color='white' animation='tada-hover' name='medium' type='logo' ></box-icon>
-            </a> 
+        <a href="https://www.facebook.com/canadascapitalhackathon" target="_blank">
+          <box-icon size='sm' color='white' animation='tada-hover' type="logo" name="facebook-square"/>
+        </a>
+        <a href="https://twitter.com/hackthehiII" target="_blank">
+          <box-icon size='sm' color='white' animation='tada-hover' name="twitter" type="logo" />
+        </a>
+        <a href="https://www.instagram.com/hackthehill/" target="_blank">
+          <box-icon size='sm' color='white' animation='tada-hover' name="instagram" type="logo" />
+        </a>
+        <a href="https://www.tiktok.com/@hackthehill" target="_blank">
+          <box-icon size='sm' color='white' animation='tada-hover' name="tiktok" type="logo" />
+        </a>
+        <a href="https://www.linkedin.com/company/hackthehill/" target="_blank">
+          <box-icon size='sm' color='white' animation='tada-hover' name='linkedin' type='logo' ></box-icon>
+        </a>
+        <a href="https://www.medium.com/@hackthehill" target="_blank">
+          <box-icon size='sm' color='white' animation='tada-hover' name='medium' type='logo' ></box-icon>
+        </a> 
       </div>   
     </Menu>
     </div>
