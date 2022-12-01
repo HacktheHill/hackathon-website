4c2ba4bdeafd16d5b445b0a3499a205dc108dbd5
Daniel Thorp <arcanistzed@gmail.com>
2022-12-01 15:11:58 -0500

    Update hero page (#39)
    * updated bg, needs fix for mobile and mlh logo
    
    * Animated hero section and simpler/more responsive
    
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>
    
    * Fix for iOS Safari
    
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>
    
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>
    Co-authored-by: Arman <arman.dogru@gmail.com>

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,66 +1,21 @@
-import './Hero.css';
-import { Box, Button, useMediaQuery, useTheme, darken } from '@mui/material';
-import HeroImg from '../../assets/svgs/parliament-hero-asset.svg';
-import BannerLogo from '../../assets/Logos/hthlogo_banner_ver.svg';
-import { Link } from 'react-scroll';
+import "./Hero.css";
+import { ReactComponent as HeroImg } from "../../assets/svgs/hth_hero_withoutbg.svg";
+import BannerLogo from "../../assets/Logos/hthlogo_banner_ver.svg";
 
 function Hero() {
-
-  const theme = useTheme();
-  const matchesSM = useMediaQuery(theme.breakpoints.down("sm"))
-  const matchesMD = useMediaQuery(theme.breakpoints.down("md"))
-  const matchesLG = useMediaQuery(theme.breakpoints.down("lg"))
-
-  return (
-    <div className="Hero">
-      <Box sx={{
-        width: '100%',
-        marginTop: matchesSM ? '2%' : '3%',
-        transition: '1s'
-      }}>
-        <Box sx={{
-          marginLeft: '5%',
-          width: matchesSM ? '85%' : '50%',
-          position: matchesSM ? 'relative' : 'absolute',
-          transition: '1s'
-        }}>
-            <Box>
-              <h3>University of Ottawa</h3>
-              <img class='BannerLogo'src={BannerLogo}></img>
-              <h3>Canada's Capital Hackathon</h3>
-            </Box>
-            <Box sx={{
-              display:"flex"
-            }}>
-              <Button target='_blank' href='https://hackthehill.com/register' sx={{
-                  background:'#5C71AD',
-                  borderRadius:'100px',
-                  color:'white',
-                  mt: matchesSM ? '7vw' : '2vw',
-                  p:'10px',
-                  textTransform:'none',
-                  minWidth:'max(10rem, 20%)',
-                  width: matchesMD ? '40%' : '0%',
-                  transition: '1s',
-                  boxShadow: 'rgba(0, 0, 0, 0.15) 0px 15px 25px, rgba(0, 0, 0, 0.05) 0px 5px 10px',
-                  fontSize: '25px',
-                  '&:hover': {
-                    backgroundColor: darken('#5C71AD', 0.3)}
-                  }}
-                >
-                  <p class='ButtonText'>Register Now!</p>
-                </Button>
-            </Box>
-        </Box>
-        <Box sx={{
-          mt: matchesSM ? '10%' : '0%',
-          transition: '1s',
-        }}>
-          <img class="HeroImg" src={HeroImg}></img>
-        </Box>
-      </Box>
-    </div>
-  );
+	return (
+		<>
+			<div className="HeroHeading">
+				<h3>University of Ottawa</h3>
+				<img class="BannerLogo" src={BannerLogo} alt="Hack the Hill"></img>
+				<h3>Canada's Capital Hackathon</h3>
+				<a target="_blank" href="https://hackthehill.com/register" rel="noreferrer">
+					<button className="HeroButton">Register Now!</button>
+				</a>
+			</div>
+			<HeroImg className="HeroImg" />
+		</>
+	);
 }
 
 export default Hero;
