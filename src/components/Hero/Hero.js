06c278ce63bec5551dc341ef30ad0fb3edcfe7c8
Emilien Breton <66650633+Bricktech2000@users.noreply.github.com>
2022-12-02 14:23:21 -0500

    changed hero tagline content (#46)
    * changed hero tagline content
    
    * prettified `Hero.js`

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,24 +1,30 @@
-import "./Hero.css";
-import { ReactComponent as HeroImg } from "../../assets/svgs/hth_hero_withoutbg.svg";
-import BannerLogo from "../../assets/Logos/hthlogo_banner_ver.svg";
+import './Hero.css';
+import { ReactComponent as HeroImg } from '../../assets/svgs/hth_hero_withoutbg.svg';
+import BannerLogo from '../../assets/Logos/hthlogo_banner_ver.svg';
 
 function Hero() {
-	return (
-		<>
-			<div className="HeroHeading">
-				<h3>
-					University of Ottawa • February <time dateTime="03/02/2023">3</time>-
-					<time datetime="05/02/2023">5</time>, 2023
-				</h3>
-				<img class="BannerLogo" src={BannerLogo} alt="Hack the Hill"></img>
-				<h3>Canada's Capital Hackathon</h3>
-				<a className="HeroButton" target="_blank" href="https://hackthehill.com/register" rel="noreferrer">
-					<button>Register Now!</button>
-				</a>
-			</div>
-			<HeroImg className="HeroImg" />
-		</>
-	);
+  return (
+    <>
+      <div className="HeroHeading">
+        <h3>
+          Canada's Capital Hackathon &nbsp;&bull;&nbsp; February{' '}
+          <time dateTime="03/02/2023">3</time>&ndash;
+          <time datetime="05/02/2023">5</time>, 2023
+        </h3>
+        <img class="BannerLogo" src={BannerLogo} alt="Hack the Hill"></img>
+        <h3>University of Ottawa &times; Carleton University</h3>
+        <a
+          className="HeroButton"
+          target="_blank"
+          href="https://hackthehill.com/register"
+          rel="noreferrer"
+        >
+          <button>Register Now!</button>
+        </a>
+      </div>
+      <HeroImg className="HeroImg" />
+    </>
+  );
 }
 
 export default Hero;
