91f633b43bbfdcda6a568c51285404427e976cf5
Daniel Thorp <arcanistzed@gmail.com>
2022-12-02 13:09:01 -0500

    Update hero page (#42)
    * Make hero more responsive for shorter screens
    
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>
    
    * SVGs for banner, icon, & hero (w/ animations)
    
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>
    
    * Add date to hero page
    
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>
    
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,11 +6,14 @@ function Hero() {
 	return (
 		<>
 			<div className="HeroHeading">
-				<h3>University of Ottawa</h3>
+				<h3>
+					University of Ottawa • February <time dateTime="03/02/2023">3</time>-
+					<time datetime="05/02/2023">5</time>, 2023
+				</h3>
 				<img class="BannerLogo" src={BannerLogo} alt="Hack the Hill"></img>
 				<h3>Canada's Capital Hackathon</h3>
-				<a target="_blank" href="https://hackthehill.com/register" rel="noreferrer">
-					<button className="HeroButton">Register Now!</button>
+				<a className="HeroButton" target="_blank" href="https://hackthehill.com/register" rel="noreferrer">
+					<button>Register Now!</button>
 				</a>
 			</div>
 			<HeroImg className="HeroImg" />
