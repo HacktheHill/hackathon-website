10e01d18e1973570eecb3df1f0ad133243269b3c
Daniel Thorp <arcanistzed@gmail.com>
2022-12-03 17:16:26 -0500

    Sticky navbar and hero improvements (#48)
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>
    
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,21 +1,104 @@
-import './NavBar.css';
-import { Link } from 'react-scroll';
-import MLH from '../../assets/Logos/mlhbadge.svg';
-function Navbar() {
-  return (
-    <div className="Navbar">
-        <img className='logo' alt='Logo' src="Logos\hthlogo_icon_ver.svg"></img>
-        <div className='Items'>
-          <a target="_blank" href="https://mlh.io/seasons/2023/events"><img className='mlh' alt="MLH" src={MLH}></img></a>
-          <div className='Item'><Link to="FAQ" spy={true} smooth={true} offset={-30} duration={500}>FAQ</Link></div>
-          <div className='Item'><Link to="Collaborators" spy={true} smooth={true} offset={-30} duration={500}>Collaborators</Link></div>
-          <div className='Item'><Link to="Schedule" spy={true} smooth={true} offset={0} duration={500}>The Hacker Series</Link></div>
-          <div className='Item'><Link to="Sponsors" spy={true} smooth={true} offset={-30} duration={500}>Sponsors</Link></div>
-          <div className='Item'><Link to="About" spy={true} smooth={true} offset={-30} duration={500}>About</Link></div> 
-        </div>
-        
-    </div>
-  );
+import "./NavBar.css";
+import { Link } from "react-scroll";
+import MLH from "../../assets/Logos/mlhbadge.svg";
+
+function Navbar({ pageScroll }) {
+	return (
+		<nav className="navbar" data-scrolled={pageScroll > 100}>
+			<ul>
+				<li className="logo">
+					<Link
+						className="link"
+						activeClass="active"
+						to="Hero"
+						spy={true}
+						smooth={true}
+						offset={0}
+						duration={500}
+					>
+						<img alt="Logo" src="Logos\hthlogo_icon_ver.svg"></img>
+					</Link>
+				</li>
+				<li>
+					<Link
+						className="link"
+						activeClass="active"
+						to="About"
+						spy={true}
+						smooth={true}
+						offset={-120}
+						duration={500}
+					>
+						About
+					</Link>
+				</li>
+				<li>
+					<Link
+						className="link"
+						activeClass="active"
+						to="Sponsors"
+						spy={true}
+						smooth={true}
+						offset={-120}
+						duration={500}
+					>
+						Sponsors
+					</Link>
+				</li>
+				<li>
+					<Link
+						className="link"
+						activeClass="active"
+						to="Schedule"
+						spy={true}
+						smooth={true}
+						offset={-80}
+						duration={500}
+					>
+						The Hacker Series
+					</Link>
+				</li>
+				<li>
+					<Link
+						className="link"
+						activeClass="active"
+						to="Collaborators"
+						spy={true}
+						smooth={true}
+						offset={-120}
+						duration={500}
+					>
+						Collaborators
+					</Link>
+				</li>
+
+				<li>
+					<Link
+						className="link"
+						activeClass="active"
+						to="FAQ"
+						spy={true}
+						smooth={true}
+						offset={-120}
+						duration={500}
+					>
+						FAQ
+					</Link>
+				</li>
+				<li className="mlh">
+					<Link
+						className="link"
+						activeClass="active"
+						to="https://mlh.io/seasons/2023/events"
+						smooth={true}
+						duration={1000}
+					>
+						<img alt="MLH" src={MLH}></img>
+					</Link>
+				</li>
+			</ul>
+		</nav>
+	);
 }
 
 export default Navbar;
