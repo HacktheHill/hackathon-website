d50d65896a0c21e264961c5873d45f5c961fb895
Daniel Thorp <arcanistzed@gmail.com>
2022-12-02 22:19:30 -0500

    Add noreferrer to links
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -16,32 +16,32 @@ function Footer() {
         </div>
         <div class="FooterContainer">
           <div className="MediaLinks">
-            <a href="https://www.facebook.com/canadascapitalhackathon" target="_blank">
+            <a href="https://www.facebook.com/canadascapitalhackathon" target="_blank" rel="noreferrer">
               <box-icon color='#3B4779' animation='tada-hover' type="logo" name="facebook-square"/>
             </a>
-            <a href="https://twitter.com/hackthehiII" target="_blank">
+            <a href="https://twitter.com/hackthehiII" target="_blank" rel="noreferrer">
               <box-icon color='#3B4779' animation='tada-hover' name="twitter" type="logo" />
             </a>
-            <a href="https://www.instagram.com/hackthehill/" target="_blank">
+            <a href="https://www.instagram.com/hackthehill/" target="_blank" rel="noreferrer">
               <box-icon color='#3B4779' animation='tada-hover' name="instagram" type="logo" />
             </a>
-            <a href="https://www.tiktok.com/@hackthehill" target="_blank">
+            <a href="https://www.tiktok.com/@hackthehill" target="_blank" rel="noreferrer">
               <box-icon color='#3B4779' animation='tada-hover' name="tiktok" type="logo" />
             </a>
-            <a href="https://www.linkedin.com/company/hackthehill/" target="_blank">
+            <a href="https://www.linkedin.com/company/hackthehill/" target="_blank" rel="noreferrer">
               <box-icon color='#3B4779' animation='tada-hover' name='linkedin' type='logo' ></box-icon>
             </a>
-            <a href="https://www.medium.com/@hackthehill" target="_blank">
+            <a href="https://www.medium.com/@hackthehill" target="_blank" rel="noreferrer">
               <box-icon color='#3B4779' animation='tada-hover' name='medium' type='logo' ></box-icon>
             </a>
           </div>
           <div className='FooterLinks'>
-            <a href={MLH_code_of_conduct} target="_blank">Code Of Conduct</a>
-            <a href="https://mlh.io/privacy" target="_blank">Privacy Policy</a>
+            <a href={MLH_code_of_conduct} target="_blank" rel="noreferrer">Code Of Conduct</a>
+            <a href="https://mlh.io/privacy" target="_blank" rel="noreferrer">Privacy Policy</a>
             {/* <a href={MLH_code_of_conduct}>Hack the Hill Constitution</a> */}
           </div>
         </div>
-        
+
       </div>
     </div>
   );
