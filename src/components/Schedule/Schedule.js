1846d75c9ad317fc54f9792dee4bf5f89d9741c3
disala <disala.desilva@gmail.com>
2022-12-04 01:46:53 -0500

    Fix schedule details (#53) (#54)
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>
    Co-authored-by: Daniel Thorp <arcanistzed@gmail.com>

--- src/components/Schedule/Schedule.js
+++ src/components/Schedule/Schedule.js
@@ -178,63 +178,13 @@ function Schedule() {
                   }
                   }}
                 >
-                  <p class='ButtonText'>View Video</p>
+                  <p class='ButtonText'>Sign Up <b>→</b></p>
                 </Button>
               </div>
               <div className ="subTitle">6 p.m. - 10 p.m. @ STM 117</div>
               <p className="eventDesc" align='left'>
               Explore the wonders of front-end development! Learn the basics through workshops and participate
               in a one-hour challenge to build a website according to provided guidelines.
-              </p>
-            </Stack>
-          </Stack>
-
-          <Stack direction = {{xs: "column", md: "row"}} spacing={matchesMD ? 5 : 10} sx={{ width:'100%' }}>
-            <div class='Center'>
-              <Card
-                variant='outlined'
-                elevation={4}
-                style={styles.card}
-                sx={{
-                  '&:hover': {
-                    transform:'scale(1.05)',
-                    transition: 'transform 330ms ease-in-out'
-                }
-                }}
-              >
-                <Stack direction={'column'}>
-                  <div className ="eventMonth">January</div>
-                  <div className ="eventDay">25</div>
-                </Stack>
-              </Card>
-            </div>
-            <Stack sx={{ width:'100%' }}>
-              <div className ="eventTitle">
-                Got Games?
-                <Button disabled sx={{
-                  background:'linear-gradient(90deg, #ABEFFB, transparent) #E9D9F2',
-                  borderRadius:'100px',
-                  color:'#3B4779',
-                  mt: matchesSM ? '7vw' : matchesMD ? '3vw' : '0vw',
-                  mb: matchesSM ? '7vw' : matchesMD ? '3vw' : '0vw',
-                  p:'7px',
-                  textTransform:'none',
-                  minWidth:'16%',
-                  width: matchesSM ? '50%' : matchesLG ? '25%' : '0%',
-                  transition: '1s',
-                  '&:hover': {
-                    backgroundColor: '#f094bc',
-                  }
-                  }}
-                >
-                  <p class='ButtonText'>Sign up <b>→</b></p>
-                </Button>
-              </div>
-              <div className ="subTitle">7 p.m. - 9 p.m. @ STM 117</div>
-              <p className="eventDesc" align='left'>
-                Discover the best that game development has to offer!
-                Join us alongside the uOttawa Game Development Club for an introduction to Unity workshop,
-                presented by Propel VR.
               </p>
             </Stack>
           </Stack>
