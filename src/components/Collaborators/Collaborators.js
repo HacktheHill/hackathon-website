a8eb8385d8b5997c0fc12b0ffdbb63d65380f0ef
Daniel Thorp <arcanistzed@gmail.com>
2022-12-05 16:44:52 -0500

    Cleanup collaborators section leftover CSS & Remove MLH (#57)
    * Cleanup collaborators section leftover CSS (#56)
    
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>
    
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>
    
    * Remove MLH and align navbar to right
    
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>
    
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- src/components/Collaborators/Collaborators.js
+++ src/components/Collaborators/Collaborators.js
@@ -1,21 +1,13 @@
-import './Collaborators.css';
-import { Grid, useMediaQuery, useTheme } from "@mui/material";
-import mountain from '../../assets/Logos/mountain_flags.svg'
+import "./Collaborators.css";
+import Mountain from "../../assets/Logos/mountain_flags.svg";
 
 function Collaborators() {
-  const theme = useTheme();
-  const matchesSM = useMediaQuery(theme.breakpoints.down("sm"));
-  const matchesMD = useMediaQuery(theme.breakpoints.down("md"));
-  const matchesLG = useMediaQuery(theme.breakpoints.down("lg"));
-  return (
-    <div id="Collaborators" className="Collaborators">
-       <h1>Collaborators </h1>
-        <Grid container id="Associations" className="Associations">
-          <img className="mountain" alt="mountain" src={mountain}></img>
-          </Grid>
-    </div>
-
-  );
+	return (
+		<div id="Collaborators" className="Collaborators">
+			<h1>Collaborators </h1>
+			<img src={Mountain} className="Mountain" alt="Collaborators mountain" />
+		</div>
+	);
 }
 
 export default Collaborators;
