69ad08b0777966911e470cadabf3ad88248e45f3
Daniel Thorp <arcanistzed@gmail.com>
2023-01-05 22:58:34 -0500

    Countdown (#67) (#68)
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,35 @@
+import { useEffect, useState } from "react";
 import hero from "../../assets/hero.svg?raw";
 import "./Hero.css";
 import BannerLogo from "/Logos/hackthehill-banner.svg";
 import LocationPin from "/SVGs/location-pin.svg";
 
+const date = new Date("2023-02-03T00:00:00-05:00");
+
 function Hero() {
+	const [popupOpen, setPopupOpen] = useState(false);
+	const [time, setTime] = useState(Date.now());
+
+	useEffect(() => {
+		const interval = setInterval(() => setTime(Date.now()), 1000);
+		return () => clearInterval(interval);
+	}, []);
+
+	const days = Math.floor((date - time) / 1000 / 60 / 60 / 24);
+	const hours = Math.floor((date - time) / 1000 / 60 / 60) % 24;
+	const minutes = Math.floor((date - time) / 1000 / 60) % 60;
+	const seconds = Math.floor((date - time) / 1000) % 60;
+
+	const popup = event => {
+		if (event.target.closest("#clock-tower")) {
+			setPopupOpen(true);
+		} else {
+			setPopupOpen(false);
+		}
+	};
+
 	return (
-		<div className="Hero">
+		<div className="Hero" onPointerMove={popup} onTouchStart={popup}>
 			<div className="HeroHeading">
 				<div className="LocationDateHeading">
 					<h3>
@@ -33,6 +57,24 @@ function Hero() {
 					__html: hero,
 				}}
 			></div>
+			<dialog className="CountdownDialog" open={popupOpen}>
+				<div className="CountdownItem">
+					<h3>{days}</h3>
+					<h4>day{days === 1 ? "" : "s"}</h4>
+				</div>
+				<div className="CountdownItem">
+					<h3>{hours}</h3>
+					<h4>hour{hours === 1 ? "" : "s"}</h4>
+				</div>
+				<div className="CountdownItem">
+					<h3>{minutes}</h3>
+					<h4>minute{minutes === 1 ? "" : "s"}</h4>
+				</div>
+				<div className="CountdownItem">
+					<h3>{seconds}</h3>
+					<h4>second{seconds === 1 ? "" : "s"}</h4>
+				</div>
+			</dialog>
 		</div>
 	);
 }
