d8b290c9730550d325f62f2e31515275c08c0f77
Daniel Thorp <arcanistzed@gmail.com>
2023-01-16 03:05:20 -0500

    feat: github link in footer
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,12 @@
-import { faFacebook, faInstagram, faLinkedin, faMedium, faTiktok, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import {
+	faFacebook,
+	faGithub,
+	faInstagram,
+	faLinkedin,
+	faMedium,
+	faTiktok,
+	faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon as Icon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import "./Footer.css";
@@ -7,59 +15,58 @@ import FooterLogo from "/Logos/hackthehill-banner.svg";
 function Footer() {
 	return (
 		<div className="Footer">
-			<div className="FooterBody">
-				<div className="FooterLogo">
-					<img src={FooterLogo} alt="Logo" />
+			<div className="FooterLogo">
+				<img src={FooterLogo} alt="Logo" />
+			</div>
+			<div className="LogoText">
+				<p>Canada's Capital Hackathon</p>
+			</div>
+			<div className="FooterContainer">
+				<div className="MediaLinks">
+					<a
+						href="https://www.facebook.com/canadascapitalhackathon"
+						target="_blank"
+						rel="noreferrer"
+						aria-label="Facebook">
+						<Icon icon={faFacebook} />
+					</a>
+					<a href="https://twitter.com/hackthehiII" target="_blank" rel="noreferrer" aria-label="Twitter">
+						<Icon icon={faTwitter} />
+					</a>
+					<a
+						href="https://www.instagram.com/hackthehill/"
+						target="_blank"
+						rel="noreferrer"
+						aria-label="Instagram">
+						<Icon icon={faInstagram} />
+					</a>
+					<a href="https://www.tiktok.com/@hackthehill" target="_blank" rel="noreferrer" aria-label="TikTok">
+						<Icon icon={faTiktok} />
+					</a>
+					<a
+						href="https://www.linkedin.com/company/hackthehill/"
+						target="_blank"
+						rel="noreferrer"
+						aria-label="LinkedIn">
+						<Icon icon={faLinkedin} />
+					</a>
+					<a href="https://www.medium.com/@hackthehill" target="_blank" rel="noreferrer" aria-label="Medium">
+						<Icon icon={faMedium} />
+					</a>
 				</div>
-				<div className="LogoText">
-					<p>Canada's Capital Hackathon</p>
-				</div>
-				<div className="FooterContainer">
-					<div className="MediaLinks">
-						<a
-							href="https://www.facebook.com/canadascapitalhackathon"
-							target="_blank"
-							rel="noreferrer"
-							aria-label="Facebook">
-							<Icon icon={faFacebook} />
-						</a>
-						<a href="https://twitter.com/hackthehiII" target="_blank" rel="noreferrer" aria-label="Twitter">
-							<Icon icon={faTwitter} />
-						</a>
-						<a
-							href="https://www.instagram.com/hackthehill/"
-							target="_blank"
-							rel="noreferrer"
-							aria-label="Instagram">
-							<Icon icon={faInstagram} />
-						</a>
-						<a
-							href="https://www.tiktok.com/@hackthehill"
-							target="_blank"
-							rel="noreferrer"
-							aria-label="TikTok">
-							<Icon icon={faTiktok} />
-						</a>
-						<a
-							href="https://www.linkedin.com/company/hackthehill/"
-							target="_blank"
-							rel="noreferrer"
-							aria-label="LinkedIn">
-							<Icon icon={faLinkedin} />
-						</a>
-						<a
-							href="https://www.medium.com/@hackthehill"
-							target="_blank"
-							rel="noreferrer"
-							aria-label="Medium">
-							<Icon icon={faMedium} />
-						</a>
-					</div>
-				</div>
-				<div className="FooterText">
-					<p>Control the Capital, Command the Cabinet, Build until you Hack the Hill</p>
-					<small>&copy; 2022-2023 Hack the Hill</small>
-				</div>
+			</div>
+			<div className="FooterText">
+				<p>Control the Capital, Command the Cabinet, Build until you Hack the Hill</p>
+				<a
+					href="https://github.com/HacktheHill/2023.hackthehill.com"
+					target="_blank"
+					rel="noreferrer"
+					aria-label="GitHub"
+					className="FooterLink">
+					<Icon icon={faGithub} />
+					Source Code
+				</a>
+				<small>&copy; 2022-2023 Hack the Hill</small>
 			</div>
 		</div>
 	);
