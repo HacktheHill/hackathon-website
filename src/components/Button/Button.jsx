02bf16f6ad7ace86b3d8b7e3e35027a957de28c0
saheeen-jey <saheen_j@ymail.com>
2023-02-14 02:17:25 -0500

    feat: button component added

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./Button.css";
+function InnerButton({ children, ...rest }) {
+    return (
+      <button {...rest}>
+        {children}
+      </button>
+    );
+  }
+  
+  export default function Button({ children, href, target, ...rest }) {
+    return href ? (
+      <a
+        href={href}
+        target={target}
+        referrer={target === "_blank" ? "noreferrer" : ""}
+        className="button"
+      >
+        <InnerButton {...rest}>{children}</InnerButton>
+      </a>
+    ) : (
+        <InnerButton {...rest}>{children}</InnerButton>
+    );
+  }
