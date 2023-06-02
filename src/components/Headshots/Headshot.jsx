60ad0b13c5089c6cd8b125bda9e614ac9e4d923c
[Hiba] <htant016@uottawa.ca>
2023-06-01 21:03:16 -0400

    fix: trying

--- src/components/Headshots/Headshot.jsx
+++ src/components/Headshots/Headshot.jsx
@@ -5,7 +5,7 @@ import { FontAwesomeIcon as Icon } from "@fortawesome/react-fontawesome";
 import "./Headshot.css";
 
 const Headshot = ({ team, name, role, instagram, linkedin, website }) => {
-	const path = `./headshots/${team}/${name}.webp`;
+	const path = `./${name}.webp`;
 	const alt = `image of ${name}`;
 	role = role ?? "";
 
@@ -33,7 +33,7 @@ const Headshot = ({ team, name, role, instagram, linkedin, website }) => {
 	return (
 		<div className="card">
 			<div className="card-content">
-				<div className="wrapper">
+				<div className="front">
 					<img className="headshot" src={path} alt={alt} />
 				</div>
 
