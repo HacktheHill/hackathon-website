4961573d9e930ffe464a0aa896520696c9bd4560
[Hiba] <htant016@uottawa.ca>
2023-06-05 14:50:15 -0400

    fix: fixed images

--- src/components/Headshots/Headshot.jsx
+++ src/components/Headshots/Headshot.jsx
@@ -4,14 +4,9 @@ import { faWindowMaximize } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon as Icon } from "@fortawesome/react-fontawesome";
 import "./Headshot.css";
 
-const getImagePath = (team, name) => {
-	const imagePath = `/headshots/${team}/${name}.webp`;
-	return new URL(imagePath, import.meta.url);
-  };
 
 const Headshot = ({ team, name, role, instagram, linkedin, website }) => {
-	const path = getImagePath(team, name);
-	//const path = `/headshots/${team}/${name}.webp`;
+	const path = `/headshots/${team}/${name}.webp`.toLowerCase();
 	const alt = 'image of '+ name;
 	role = role ?? "";
 
