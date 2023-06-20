5b7e34ba02b78281de6990bcd0302774c129139a
Zackydout101 <zleve009@uottawa.ca>
2023-06-19 20:39:06 -0400

    feat: sidebar and css modules
    Co-authored-by: Sacha Arseneault <sacha.arseneault@gmail.com>

--- src/components/NavBar/Sidebar.jsx
+++ src/components/NavBar/Sidebar.jsx
@@ -1,114 +1,144 @@
-import { faFacebook, faInstagram, faLinkedin, faTiktok, faTwitter } from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon as Icon } from "@fortawesome/react-fontawesome";
-import React, { useCallback, useState } from "react";
-import reactBurgerMenu from "react-burger-menu";
-import { Link } from "react-scroll";
-import { t } from "../../i18n";
+import React, { useState } from "react";
 import "./Sidebar.css";
-const { slide: Menu } = reactBurgerMenu;
+import { FontAwesomeIcon as Icon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faInstagram, faLinkedin, faTiktok, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { t } from "../../i18n";
+import styles from "./Sidebar.module.css";
 
-export default function Sidebar() {
-	const [menuOpen, setMenuOpen] = useState(false);
-
-	const handleCloseMenu = useCallback(() => {
-		setMenuOpen(false);
-	}, []);
-
-	const handleStateChange = useCallback(state => {
-		setMenuOpen(state.isOpen);
-	}, []);
+const Sidebar = () => {
+	const [isNavOpen, setIsNavOpen] = useState(false);
+	const links = [
+		{
+			id: 1,
+			to: "about",
+			spy: true,
+			smooth: true,
+			offset: -30,
+			duration: 500,
+			text: t("navbar.links.about"),
+			href: "#about",
+		},
+		{
+			id: 2,
+			to: "sponsors",
+			spy: true,
+			smooth: true,
+			offset: -30,
+			duration: 500,
+			text: t("navbar.links.sponsors"),
+			href: "#sponsors",
+		},
+		{
+			id: 3,
+			to: "schedule",
+			spy: true,
+			smooth: true,
+			offset: 0,
+			duration: 500,
+			text: t("navbar.links.hacker"),
+			href: "#schedule",
+		},
+		{
+			id: 4,
+			to: "collaborators",
+			spy: true,
+			smooth: true,
+			offset: -30,
+			duration: 500,
+			text: t("navbar.links.collaborators"),
+			href: "#collaborators",
+		},
+		{
+			id: 5,
+			to: "faq",
+			spy: true,
+			smooth: true,
+			offset: -30,
+			duration: 500,
+			text: t("navbar.links.faq"),
+			href: "#faq",
+		},
+	];
 
 	return (
-		<div className="sidebar">
-			<Menu right width={"100%"} isOpen={menuOpen} onStateChange={handleStateChange}>
-				<Link
-					to="about"
-					spy={true}
-					smooth={true}
-					offset={-30}
-					duration={500}
-					onClick={handleCloseMenu}
-					href="#about"
+		<>
+			<div className={styles["icon-container"]}>
+				<button
+					className={` ${ styles["sidebar-icon"]} ${isNavOpen ?  styles["sidebar-change"] : null}`}
+					onClick={() => setIsNavOpen(!isNavOpen)}
 				>
-					{t("navbar.links.about")}
-				</Link>
-				<Link
-					to="sponsors"
-					spy={true}
-					smooth={true}
-					offset={-30}
-					duration={500}
-					onClick={handleCloseMenu}
-					href="#sponsors"
-				>
-					{t("navbar.links.sponsors")}
-				</Link>
-				<Link
-					to="schedule"
-					spy={true}
-					smooth={true}
-					offset={0}
-					duration={500}
-					onClick={handleCloseMenu}
-					href="#schedule"
-				>
-					{t("navbar.links.hacker")}
-				</Link>
-				<Link
-					to="collaborators"
-					spy={true}
-					smooth={true}
-					offset={-30}
-					duration={500}
-					onClick={handleCloseMenu}
-					href="#collaborators"
-				>
-					{t("navbar.links.collaborators")}
-				</Link>
-				<Link
-					to="faq"
-					spy={true}
-					smooth={true}
-					offset={-30}
-					duration={500}
-					onClick={handleCloseMenu}
-					href="#faq"
-				>
-					{t("navbar.links.faq")}
-				</Link>
-				<div className="media-links">
-					<a
-						href="https://www.facebook.com/canadascapitalhackathon"
-						target="_blank"
-						rel="noreferrer"
-						aria-label="Facebook"
-					>
-						<Icon icon={faFacebook} />
-					</a>
-					<a href="https://twitter.com/hackthehill_" target="_blank" rel="noreferrer" aria-label="Twitter">
-						<Icon icon={faTwitter} />
-					</a>
-					<a
-						href="https://www.instagram.com/hackthehill/"
-						target="_blank"
-						rel="noreferrer"
-						aria-label="Instagram"
-					>
-						<Icon icon={faInstagram} />
-					</a>
-					<a href="https://www.tiktok.com/@hackthehill" target="_blank" rel="noreferrer" aria-label="TikTok">
-						<Icon icon={faTiktok} />
-					</a>
-					<a
-						href="https://www.linkedin.com/company/hackthehill/"
-						target="_blank"
-						rel="noreferrer"
-						aria-label="LinkedIn"
-					>
-						<Icon icon={faLinkedin} />
-					</a>
-				</div>
-			</Menu>
-		</div>
+					<div className={styles.bar1}></div>
+        <div className={styles.bar2}></div>
+        <div className={styles.bar3}></div>
+				</button>
+			</div>
+			<div className={styles["sidebar-content"]}>
+				<nav className={`${ styles.nav} ${isNavOpen ? styles["nav-open"] : styles["nav-closed"]}`}>
+					<ul className={styles["nav-links"]}>
+						{links.map(link => (
+							<li className={styles.link} key={link.text}>
+								<a
+									href={`${link.href}`}
+									onClick={() => setIsNavOpen(!isNavOpen)}
+									style={{
+										fontFamily: "Rubik",
+										textAlign: "center",
+										textDecoration: "none",
+										color: "black",
+									}}
+								>
+									{link.text}
+								</a>
+							</li>
+						))}
+					</ul>
+
+					<div className={styles["media-links"]}>
+						<a
+							href="https://www.facebook.com/canadascapitalhackathon"
+							target="_blank"
+							rel="noreferrer"
+							aria-label="Facebook"
+						>
+							<Icon icon={faFacebook} size="2x" />
+						</a>
+						<a
+							href="https://twitter.com/hackthehill_"
+							target="_blank"
+							rel="noreferrer"
+							aria-label="Twitter"
+						>
+							<Icon icon={faTwitter} size="2x" />
+						</a>
+						<a
+							href="https://www.instagram.com/hackthehill/"
+							target="_blank"
+							rel="noreferrer"
+							aria-label="Instagram"
+						>
+							<Icon icon={faInstagram} size="2x" />
+						</a>
+						<a
+							href="https://www.tiktok.com/@hackthehill"
+							target="_blank"
+							rel="noreferrer"
+							aria-label="TikTok"
+						>
+							<Icon icon={faTiktok} size="2x" />
+						</a>
+						<a
+							href="https://www.linkedin.com/company/hackthehill/"
+							target="_blank"
+							rel="noreferrer"
+							aria-label="LinkedIn"
+						>
+							<Icon icon={faLinkedin} size="2x" />
+						</a>
+					</div>
+				</nav>
+			</div>
+		</>
 	);
-}
+};
+
+export default Sidebar;
