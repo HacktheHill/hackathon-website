db5687999570513227f5eda01f2a73b3fe99fc21
[Hiba] <htant016@uottawa.ca>
2023-07-14 23:28:04 -0400

    remove: removed footer-container in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,54 +12,53 @@ function Footer() {
 				<img src={FooterLogo} alt="Logo" />
 			</div>
 			<p className="logo-text">{t("footer.title")}</p>
-			<div className="footer-container">
-				<div className="media-links">
-					<a
-						href="https://www.facebook.com/canadascapitalhackathon"
-						target="_blank"
-						rel="noreferrer"
-						aria-label="Facebook"
-					>
-						<Icon icon={faFacebook} />
-					</a>
-					<a href="https://twitter.com/hackthehill_" target="_blank" rel="noreferrer" aria-label="Twitter">
-						<Icon icon={faTwitter} />
-					</a>
-					<a
-						href="https://www.instagram.com/hackthehill/"
-						target="_blank"
-						rel="noreferrer"
-						aria-label="Instagram"
-					>
-						<Icon icon={faInstagram} />
-					</a>
-					<a href="https://www.tiktok.com/@hackthehill" target="_blank" rel="noreferrer" aria-label="TikTok">
-						<Icon icon={faTiktok} />
-					</a>
-					<a
-						href="https://www.linkedin.com/company/hackthehill/"
-						target="_blank"
-						rel="noreferrer"
-						aria-label="LinkedIn"
-					>
-						<Icon icon={faLinkedin} />
-					</a>
-				</div>
-			</div>
-			<div className="footer-text">
-				<p>{t("footer.description")}</p>
+
+			<div className="media-links">
 				<a
-					href="https://github.com/HacktheHill/2023.hackthehill.com"
+					href="https://www.facebook.com/canadascapitalhackathon"
 					target="_blank"
 					rel="noreferrer"
-					aria-label="GitHub"
-					className="footer-link"
+					aria-label="Facebook"
 				>
-					<Icon icon={faGithub} />
-					{t("footer.github")}
+					<Icon icon={faFacebook} />
 				</a>
-				<small>&copy; 2022-2023 Hack the Hill</small>
+				<a href="https://twitter.com/hackthehill_" target="_blank" rel="noreferrer" aria-label="Twitter">
+					<Icon icon={faTwitter} />
+				</a>
+				<a
+					href="https://www.instagram.com/hackthehill/"
+					target="_blank"
+					rel="noreferrer"
+					aria-label="Instagram"
+				>
+					<Icon icon={faInstagram} />
+				</a>
+				<a href="https://www.tiktok.com/@hackthehill" target="_blank" rel="noreferrer" aria-label="TikTok">
+					<Icon icon={faTiktok} />
+				</a>
+				<a
+					href="https://www.linkedin.com/company/hackthehill/"
+					target="_blank"
+					rel="noreferrer"
+					aria-label="LinkedIn"
+				>
+					<Icon icon={faLinkedin} />
+				</a>
 			</div>
+
+			<p className="footer-desc">{t("footer.description")}</p>
+			<a
+				href="https://github.com/HacktheHill/2023.hackthehill.com"
+				target="_blank"
+				rel="noreferrer"
+				aria-label="GitHub"
+				className="footer-link"
+			>
+				<Icon icon={faGithub} />
+				{t("footer.github")}
+			</a>
+
+			<small>&copy; 2022-2023 Hack the Hill</small>
 		</div>
 	);
 }
