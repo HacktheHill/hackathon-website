a07eae5c213498890c082aaf9b2a67647bb5cb21
Daniel Thorp <arcanistzed@gmail.com>
2023-08-14 04:48:48 -0400

    feat: simplify schedule and improve responsivity
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- src/components/Schedule/Schedule.jsx
+++ src/components/Schedule/Schedule.jsx
@@ -5,117 +5,81 @@ import styles from "./Schedule.module.css";
 function Schedule() {
 	return (
 		<div id="schedule" className={styles["schedule"]}>
-			<div className={styles["schedule-content"]}>
-				<div className={styles["schedule-title"]} align="left">
-					{t("schedule.title")}
+			<div className={styles["schedule-title"]}>{t("schedule.title")}</div>
+			<div className={styles["schedule-list"]}>
+				<div className={styles["event-card"]}>
+					<div className={styles["event-month"]}>{t("schedule.event1.month")}</div>
+					<div className={styles["event-day"]}>16</div>
 				</div>
-				<meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-				<div className={styles["schedule-box"]}>
-					<div id="eventStack" className={styles["event-stack"]}>
-						<div className={styles["event-container"]}>
-							<div className={styles["center"]}>
-								<div className={styles["event-card"]}>
-									<div className={styles["event-card-content"]}>
-										<div className={styles["event-month"]}>{t("schedule.event1.month")}</div>
-										<div className={styles["event-day"]}>16</div>
-									</div>
-								</div>
-							</div>
-							<div className={styles["event-details"]}>
-								<div className={styles["event-title"]}>
-									{t("schedule.event1.title")}
-									<button
-										className={styles["event-btn"]}
-										target="_blank"
-										href="https://youtu.be/zpixm4xz_K4"
-									>
-										<p className={styles["button-text"]}>{t("schedule.event1.status")}</p>
-									</button>
-								</div>
-								<div className={styles["sub-title"]}>{t("schedule.event1.time")}</div>
-								<p className={styles["event-desc"]}>{t("schedule.event1.description")}</p>
-							</div>
-						</div>
-
-						<div className={styles["event-container"]}>
-							<div className={styles["center"]}>
-								<div className={styles["event-card"]}>
-									<div className={styles["event-card-content"]}>
-										<div className={styles["event-month"]}>{t("schedule.event2.month")}</div>
-										<div className={styles["event-day"]}>30</div>
-									</div>
-								</div>
-							</div>
-							<div className={styles["event-details"]}>
-								<div className={styles["event-title"]}>
-									{t("schedule.event2.title")}
-									<button
-										className={styles["event-btn"]}
-										disabled
-										target="_blank"
-										href="https://forms.gle/WUgn5g8XTjNf9Eq39"
-									>
-										<p className={styles["button-text"]}>{t("schedule.event2.status")}</p>
-									</button>
-								</div>
-								<div className={styles["sub-title"]}>{t("schedule.event2.time")}</div>
-								<p className={styles["event-desc"]} align="left">
-									{t("schedule.event2.description")}
-								</p>
-							</div>
-						</div>
-
-						<div className={styles["event-container"]}>
-							<div className={styles["center"]}>
-								<div className={styles["event-card"]}>
-									<div className={styles["event-card-content"]}>
-										<div className={styles["event-month"]}>{t("schedule.event3.month")}</div>
-										<div className={styles["event-day"]}>11</div>
-									</div>
-								</div>
-							</div>
-							<div className={styles["event-details"]}>
-								<div className={styles["event-title"]}>
-									{t("schedule.event3.title")}
-									<button className={styles["event-btn"]} disabled>
-										<p className={styles["button-text"]}>{t("schedule.event3.status")}</p>
-									</button>
-								</div>
-								<div className={styles["sub-title"]}>{t("schedule.event3.time")}</div>
-								<p className={styles["event-desc"]} align="left">
-									{t("schedule.event3.description")}
-								</p>
-							</div>
-						</div>
-
-						<div className={styles["event-container"]}>
-							<div className={styles["center"]}>
-								<div className={styles["event-card"]}>
-									<div className={styles["event-card-content"]}>
-										<div className={styles["event-month"]}>{t("schedule.event4.month")}</div>
-										<div className={styles["event-day"]}>25</div>
-									</div>
-								</div>
-							</div>
-							<div className={styles["event-details"]}>
-								<div className={styles["event-title"]}>
-									{t("schedule.event4.title")}
-									<button
-										className={styles["event-btn"]}
-										href="https://youtu.be/mbr4ZmXtx0k"
-										target={"_blank"}
-									>
-										<p className={styles["button-text"]}>{t("schedule.event4.status")}</p>
-									</button>
-								</div>
-								<div className={styles["sub-title"]}>{t("schedule.event4.time")}</div>
-								<p className={styles["event-desc"]} align="left">
-									{t("schedule.event4.description")}
-								</p>
-							</div>
-						</div>
+				<section className={styles["event-details"]}>
+					<div className={styles["event-heading"]}>
+						<h1 className={styles["event-title"]}>{t("schedule.event1.title")}</h1>
+						<button
+							className={styles["event-button"]}
+							target="_blank"
+							rel="noreferrer"
+							href="https://youtu.be/zpixm4xz_K4"
+						>
+							{t("schedule.event1.status")}
+						</button>
 					</div>
+					<div className={styles["sub-title"]}>{t("schedule.event1.time")}</div>
+					<p className={styles["event-description"]}>{t("schedule.event1.description")}</p>
+				</section>
+
+				<div className={styles["event-card"]}>
+					<div className={styles["event-month"]}>{t("schedule.event2.month")}</div>
+					<div className={styles["event-day"]}>30</div>
 				</div>
+				<section className={styles["event-details"]}>
+					<div className={styles["event-heading"]}>
+						<h1 className={styles["event-title"]}>{t("schedule.event2.title")}</h1>
+						<button
+							className={styles["event-button"]}
+							disabled
+							target="_blank"
+							href="https://forms.gle/WUgn5g8XTjNf9Eq39"
+						>
+							{t("schedule.event2.status")}
+						</button>
+					</div>
+					<div className={styles["sub-title"]}>{t("schedule.event2.time")}</div>
+					<p className={styles["event-description"]}>{t("schedule.event2.description")}</p>
+				</section>
+
+				<div className={styles["event-card"]}>
+					<div className={styles["event-month"]}>{t("schedule.event3.month")}</div>
+					<div className={styles["event-day"]}>11</div>
+				</div>
+				<section className={styles["event-details"]}>
+					<div className={styles["event-heading"]}>
+						<h1 className={styles["event-title"]}>{t("schedule.event3.title")}</h1>
+						<button className={styles["event-button"]} disabled>
+							{t("schedule.event3.status")}
+						</button>
+					</div>
+					<div className={styles["sub-title"]}>{t("schedule.event3.time")}</div>
+					<p className={styles["event-description"]}>{t("schedule.event3.description")}</p>
+				</section>
+
+				<div className={styles["event-card"]}>
+					<div className={styles["event-month"]}>{t("schedule.event4.month")}</div>
+					<div className={styles["event-day"]}>25</div>
+				</div>
+				<section className={styles["event-details"]}>
+					<div className={styles["event-heading"]}>
+						<h1 className={styles["event-title"]}>{t("schedule.event4.title")}</h1>
+						<button
+							className={styles["event-button"]}
+							href="https://youtu.be/mbr4ZmXtx0k"
+							target={"_blank"}
+						>
+							{t("schedule.event4.status")}
+						</button>
+					</div>
+					<div className={styles["sub-title"]}>{t("schedule.event4.time")}</div>
+					<p className={styles["event-description"]}>{t("schedule.event4.description")}</p>
+				</section>
 			</div>
 		</div>
 	);
