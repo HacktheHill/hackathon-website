d6fb07bb6400b8ed7a0142721ab581dbc4eac19c
[Hiba] <htant016@uottawa.ca>
2023-08-16 23:19:40 -0400

    refactor: added loop component

--- src/components/Schedule/Schedule.jsx
+++ src/components/Schedule/Schedule.jsx
@@ -3,86 +3,47 @@ import { t } from "../../i18n";
 import styles from "./Schedule.module.css";
 
 function Schedule() {
-	return (
-		<div id="schedule" className={styles["schedule"]}>
-			<div className={styles["schedule-title"]}>{t("schedule.title")}</div>
-			<div className={styles["schedule-list"]}>
-				<div className={styles["event-card"]}>
-					<div className={styles["event-month"]}>{t("schedule.event1.month")}</div>
-					<div className={styles["event-day"]}>16</div>
-				</div>
-				<section className={styles["event-details"]}>
-					<div className={styles["event-heading"]}>
-						<h1 className={styles["event-title"]}>{t("schedule.event1.title")}</h1>
-						<button
-							className={styles["event-button"]}
-							target="_blank"
-							rel="noreferrer"
-							href="https://youtu.be/zpixm4xz_K4"
-						>
-							{t("schedule.event1.status")}
-						</button>
-					</div>
-					<div className={styles["sub-title"]}>{t("schedule.event1.time")}</div>
-					<p className={styles["event-description"]}>{t("schedule.event1.description")}</p>
-				</section>
 
-				<div className={styles["event-card"]}>
-					<div className={styles["event-month"]}>{t("schedule.event2.month")}</div>
-					<div className={styles["event-day"]}>30</div>
-				</div>
-				<section className={styles["event-details"]}>
-					<div className={styles["event-heading"]}>
-						<h1 className={styles["event-title"]}>{t("schedule.event2.title")}</h1>
-						<button
-							className={styles["event-button"]}
-							disabled
-							target="_blank"
-							href="https://forms.gle/WUgn5g8XTjNf9Eq39"
-						>
-							{t("schedule.event2.status")}
-						</button>
-					</div>
-					<div className={styles["sub-title"]}>{t("schedule.event2.time")}</div>
-					<p className={styles["event-description"]}>{t("schedule.event2.description")}</p>
-				</section>
+	const eventsObject = t('schedule.events');
+	const eventsArray = Object.values(eventsObject);
 
-				<div className={styles["event-card"]}>
-					<div className={styles["event-month"]}>{t("schedule.event3.month")}</div>
-					<div className={styles["event-day"]}>11</div>
-				</div>
-				<section className={styles["event-details"]}>
-					<div className={styles["event-heading"]}>
-						<h1 className={styles["event-title"]}>{t("schedule.event3.title")}</h1>
-						<button className={styles["event-button"]} disabled>
-							{t("schedule.event3.status")}
-						</button>
-					</div>
-					<div className={styles["sub-title"]}>{t("schedule.event3.time")}</div>
-					<p className={styles["event-description"]}>{t("schedule.event3.description")}</p>
-				</section>
+  return (
+    <div id="schedule" className={styles["schedule"]}>
+	<div className={styles["schedule-title"]}>{t("schedule.title")}</div>
 
-				<div className={styles["event-card"]}>
-					<div className={styles["event-month"]}>{t("schedule.event4.month")}</div>
-					<div className={styles["event-day"]}>25</div>
-				</div>
-				<section className={styles["event-details"]}>
-					<div className={styles["event-heading"]}>
-						<h1 className={styles["event-title"]}>{t("schedule.event4.title")}</h1>
-						<button
-							className={styles["event-button"]}
-							href="https://youtu.be/mbr4ZmXtx0k"
-							target={"_blank"}
-						>
-							{t("schedule.event4.status")}
-						</button>
-					</div>
-					<div className={styles["sub-title"]}>{t("schedule.event4.time")}</div>
-					<p className={styles["event-description"]}>{t("schedule.event4.description")}</p>
-				</section>
-			</div>
-		</div>
-	);
+	<div className={styles["schedule-list"]}>
+    	{eventsArray.map((event) => (
+		
+        <div className={styles["schedule-list-item"]}>
+          <div className={styles["event-card"]}>
+            <div className={styles["event-month"]}>{event.month}</div>
+            <div className={styles["event-day"]}>{event.day}</div>
+          </div>
+          <section className={styles["event-details"]}>
+            <div className={styles["event-heading"]}>
+              <h1 className={styles["event-title"]}>{event.title}</h1>
+
+			  {event.btn_Status === "" ? (
+                <a
+                  className={styles["event-button"]}
+                  target="_blank"
+                  rel="noreferrer"
+                  href={event.link}
+                >
+                  {event.status}
+                </a>
+              ) : (
+                <a className={styles["event-button-disabled"]}>{event.status}</a>
+              )}
+            </div>
+            <div className={styles["sub-title"]}>{event.time}</div>
+            <p className={styles["event-description"]}>{event.description}</p>
+          </section>
+		  </div>
+      ))}
+	  </div>
+    </div>
+  );
 }
 
 export default Schedule;
