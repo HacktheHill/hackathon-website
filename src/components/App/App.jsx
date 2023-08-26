64072cf89041561010e3391f86adf813cdf55df0
[Hiba] <htant016@uottawa.ca>
2023-08-26 14:00:28 -0400

    style: styling the centered text

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -24,7 +24,7 @@ function App() {
 	return (
 		<>
 			<Navigation pageScroll={pageScroll} />
-			{/* <Hero /> */}
+			<Hero />
 			{/* <About /> */}
 			{/* <Stats /> */}
 			{/* <Testimonials /> */}
