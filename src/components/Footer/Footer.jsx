13ae57bd8f010eea09b8602b35f065662d3d15f6
Daniel Thorp <arcanistzed@gmail.com>
2023-10-23 05:12:59 -0400

    style: footer using grid not absolute position
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,94 +1,65 @@
-import { faFacebook, faGithub, faInstagram, faLinkedin, faTiktok, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faFacebook, faInstagram, faLinkedin, faTiktok, faTwitter } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon as Icon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { t } from "../../i18n";
 import styles from "./Footer.module.css";
-import TeamCarousel from "./TeamCarousel";
 
 function Footer() {
 	return (
 		<>
 			{/* <TeamCarousel/> */}
 			<div className={styles.footer}>
-				<div className={styles["bottom-footer"]}>
-					<div className={styles["footer-link-group"]}>
-						<a
-							href="./assets/Hack-the-Hill-Constitution.pdf"
-							target="_blank"
-							rel="noreferrer"
-							aria-label="GitHub"
-							className={styles["footer-link"]}
-						>
-							{t("footer.constitution")}
-						</a>
-						<a
-							href="https://cdn1.hackthehill.com/legal/privacy-policy.pdf"
-							target="_blank"
-							rel="noreferrer"
-							aria-label="GitHub"
-							className={styles["footer-link"]}
-						>
-							{t("footer.privacy")}
-						</a>
-						<a
-							href="https://github.com/HacktheHill"
-							target="_blank"
-							rel="noreferrer"
-							aria-label="GitHub"
-							className={styles["footer-link"]}
-						>
-							{t("footer.github")}
-						</a>
-					</div>
-
-					<div className={styles["copyright-group"]}>
-						<small>&copy; 2023-2024 Hack the Hill</small>
-					</div>
+				<div className={styles["footer-links"]}>
+					<a
+						href="./assets/Hack-the-Hill-Constitution.pdf"
+						target="_blank"
+						rel="noreferrer"
+						aria-label="Constitution"
+					>
+						{t("footer.constitution")}
+					</a>
+					<a
+						href="https://cdn1.hackthehill.com/legal/privacy-policy.pdf"
+						target="_blank"
+						rel="noreferrer"
+						aria-label="Privacy Policy"
+					>
+						{t("footer.privacy")}
+					</a>
 				</div>
-				<div className={styles["media-links-container"]}>
-					<div className={styles["media-links"]}>
-						<a
-							href="https://www.facebook.com/canadascapitalhackathon"
-							target="_blank"
-							rel="noreferrer"
-							aria-label="Facebook"
-						>
-							<Icon icon={faFacebook} />
-						</a>
-						<a
-							href="https://twitter.com/hackthehill_"
-							target="_blank"
-							rel="noreferrer"
-							aria-label="Twitter"
-						>
-							<Icon icon={faTwitter} />
-						</a>
-						<a
-							href="https://www.instagram.com/hackthehill/"
-							target="_blank"
-							rel="noreferrer"
-							aria-label="Instagram"
-						>
-							<Icon icon={faInstagram} />
-						</a>
-						<a
-							href="https://www.tiktok.com/@hackthehill"
-							target="_blank"
-							rel="noreferrer"
-							aria-label="TikTok"
-						>
-							<Icon icon={faTiktok} />
-						</a>
-						<a
-							href="https://www.linkedin.com/company/hackthehill/"
-							target="_blank"
-							rel="noreferrer"
-							aria-label="LinkedIn"
-						>
-							<Icon icon={faLinkedin} />
-						</a>
-					</div>
+				<div className={styles["media-links"]}>
+					<a
+						href="https://www.facebook.com/canadascapitalhackathon"
+						target="_blank"
+						rel="noreferrer"
+						aria-label="Facebook"
+					>
+						<Icon icon={faFacebook} />
+					</a>
+					<a href="https://twitter.com/hackthehill_" target="_blank" rel="noreferrer" aria-label="Twitter">
+						<Icon icon={faTwitter} />
+					</a>
+					<a
+						href="https://www.instagram.com/hackthehill/"
+						target="_blank"
+						rel="noreferrer"
+						aria-label="Instagram"
+					>
+						<Icon icon={faInstagram} />
+					</a>
+					<a href="https://www.tiktok.com/@hackthehill" target="_blank" rel="noreferrer" aria-label="TikTok">
+						<Icon icon={faTiktok} />
+					</a>
+					<a
+						href="https://www.linkedin.com/company/hackthehill/"
+						target="_blank"
+						rel="noreferrer"
+						aria-label="LinkedIn"
+					>
+						<Icon icon={faLinkedin} />
+					</a>
 				</div>
+				<small className={styles["copyright"]}>{t("footer.copyright")}</small>
 			</div>
 		</>
 	);
