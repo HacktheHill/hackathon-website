fad909822e3f926ce04d4febcece32948af5b708
Daniel Thorp <arcanistzed@gmail.com>
2023-10-28 14:30:33 -0400

    fix: mui console error in FAQ
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- src/components/FAQ/FAQ.jsx
+++ src/components/FAQ/FAQ.jsx
@@ -128,7 +128,6 @@ export default function FAQ() {
 										t="1551322312294"
 										viewBox="0 0 1024 1024"
 										version="1.1"
-										pId="10297"
 										height="1em"
 										width="1em"
 										xmlns="http://www.w3.org/2000/svg"
@@ -136,11 +135,9 @@ export default function FAQ() {
 										<defs></defs>
 										<path
 											d="M474 152m8 0l60 0q8 0 8 8l0 704q0 8-8 8l-60 0q-8 0-8-8l0-704q0-8 8-8Z"
-											pId="10298"
 										></path>
 										<path
 											d="M168 474m8 0l672 0q8 0 8 8l0 60q0 8-8 8l-672 0q-8 0-8-8l0-60q0-8 8-8Z"
-											pId="10299"
 										></path>
 									</svg>
 								)}
@@ -207,7 +204,6 @@ export default function FAQ() {
 										t="1551322312294"
 										viewBox="0 0 1024 1024"
 										version="1.1"
-										pId="10297"
 										height="1em"
 										width="1em"
 										xmlns="http://www.w3.org/2000/svg"
@@ -215,11 +211,9 @@ export default function FAQ() {
 										<defs></defs>
 										<path
 											d="M474 152m8 0l60 0q8 0 8 8l0 704q0 8-8 8l-60 0q-8 0-8-8l0-704q0-8 8-8Z"
-											pId="10298"
 										></path>
 										<path
 											d="M168 474m8 0l672 0q8 0 8 8l0 60q0 8-8 8l-672 0q-8 0-8-8l0-60q0-8 8-8Z"
-											pId="10299"
 										></path>
 									</svg>
 								)}
