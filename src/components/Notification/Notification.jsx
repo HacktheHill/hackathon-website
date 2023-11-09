1935072c258193361b5db9402274d6975bfbb284
Daniel Thorp <arcanistzed@gmail.com>
2023-11-09 08:30:49 -0500

    feat: update notification
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -41,8 +41,8 @@ function Notification() {
 			>
 				<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 					<title>{t("notification.close")}</title>
-					<path d="M18 6L6 18" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-					<path d="M6 6L18 18" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+					<path d="M18 6L6 18" stroke="var(--light-primary-color)" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+					<path d="M6 6L18 18" stroke="var(--light-primary-color)" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
 				</svg>
 			</button>
 		</div>
