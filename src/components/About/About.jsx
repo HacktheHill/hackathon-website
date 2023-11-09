b7fa19b2014b0478a9b3557ff6fe2eed685aed33
Daniel Thorp <arcanistzed@gmail.com>
2023-11-08 22:50:25 -0500

    feat: adjust colors & youtube embed
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { t } from "../../i18n";
 import styles from "./About.module.css";
-import frame from "/SVGs/2024/about-stats/frame.svg";
+import leftLeaves from "/SVGs/2024/about-stats/left-leaves.svg";
+import rightLeaves from "/SVGs/2024/about-stats/right-leaves.svg";
 
 function About() {
 	return (
@@ -18,7 +19,17 @@ function About() {
 					{t("about.p3")}
 				</p>
 			</div>
-			<img className={styles.frame} src={frame} alt={t("about.img_alt")} />
+			<div className={styles.frame}>
+				<img className={styles["left-leaves"]} src={leftLeaves} alt="left-leaves" />
+				<iframe
+					src="https://www.youtube.com/embed/AiFOe3R3sWU?si=JEUvfaxf8jKG7uh0&controls=0&autoplay=1&loop=1&mute=1"
+					title={t("about.title")}
+					allow="accelerometer; autoplay;
+				clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+					allowfullscreen
+				></iframe>
+				<img className={styles["right-leaves"]} src={rightLeaves} alt="right-leaves" />
+			</div>
 		</div>
 	);
 }
