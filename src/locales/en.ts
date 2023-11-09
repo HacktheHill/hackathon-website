bd3fae7d98931ab8d4f2acb8443eef43407d86ff
Daniel Thorp <arcanistzed@gmail.com>
2023-11-09 06:25:22 -0500

    style: cleanup code
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -25,7 +25,7 @@ export default {
 	about: {
 		title: "Welcome to Canada's Capital Hackathon",
 		p1: "Join 1,000+ hackers from around the country to make something incredible in just 36 hours.",
-		p2: "Hack the Hill II is the second edition of Canada\'s Capital Hackathon, a three-day event where students from different disciplines come together to create innovative solutions for real-world problems",
+		p2: "Hack the Hill II is the second edition of Canada's Capital Hackathon, a three-day event where students from different disciplines come together to create innovative solutions for real-world problems",
 		p3: "Participants will be provided with opportunities to learn new skills, network with peers, and showcase their talents and ideas. Whether you are a beginner or a seasoned hacker, you will definitely find something that challenges and inspires you at Hack the Hill II.",
 	},
 	stats: {
