5e5b84b5f1c82bcea68fe2422b4089e62cc53bb6
[Hiba] <htant016@uottawa.ca>
2023-11-23 21:04:16 -0500

    style: test coulds' overflow v4

--- src/components/Schedule/Schedule.jsx
+++ src/components/Schedule/Schedule.jsx
@@ -22,7 +22,6 @@ function Schedule() {
 									left: Math.random() * 150 - 50 + "%",
 									top: Math.random() * 75 - 25 + "%",
 									transform: `scale(${Math.random() * 0.75 + 0.25})`,
-									overflowX: "hidden",
 									display: "block",
 								}}
 							></img>
