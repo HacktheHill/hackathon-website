6bc39130ff5cff1a7f130635118c58c6fecf4e14
Sacha Arseneault <sacha.arseneault@gmail.com>
2023-12-06 22:17:59 -0500

    feat: lazy loading for youtube embed

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,8 +3,13 @@ import { t } from "../../i18n";
 import styles from "./About.module.css";
 import leftLeaves from "/SVGs/About/left-leaves.svg";
 import rightLeaves from "/SVGs/About/right-leaves.svg";
+import "../../global.css";
 
 function About() {
+	const podcastLink =
+		"https://www.youtube.com/embed/videoseries?si=ZobjNMvDAoBIorPw&controls=0&list=PLvXySQVib-mmNoOeoORHRGz2UyeSEgj7Q&autoplay=0&loop=1&mute=0";
+	const thumbnailLink = "https://img.youtube.com/vi/un8x-gcfQUw/hqdefault.jpg";
+
 	return (
 		<div className={styles.about} id="about">
 			<div className={styles["about-text"]}>
@@ -16,10 +21,13 @@ function About() {
 			<div className={styles.frame}>
 				<img className={styles["left-leaves"]} src={leftLeaves} alt="left-leaves" />
 				<iframe
-					src="https://www.youtube.com/embed/videoseries?si=ZobjNMvDAoBIorPw&controls=0&list=PLvXySQVib-mmNoOeoORHRGz2UyeSEgj7Q&autoplay=0&loop=1&mute=0"
+					src={podcastLink}
 					title={t("about.title")}
+					srcDoc={`<style>*{padding:0;margin:0;overflow:hidden}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:3.5rem/1.5 sans-serif;color:#fff3b6;text-shadow:0 0 0.5em black}</style><a href=${podcastLink}><img src=${thumbnailLink} alt='Podcast'><span>▶</span></a>`}
+					frameBorder="0"
 					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
 					allowFullScreen
+					loading="lazy"
 				></iframe>
 				<img className={styles["right-leaves"]} src={rightLeaves} alt="right-leaves" />
 			</div>
