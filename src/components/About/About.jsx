c9e6a805b68f7ef1436a00301a7923c6f1a0e7b6
Daniel Thorp <arcanistzed@gmail.com>
2023-12-08 00:21:00 -0500

    fix: only click button once to play video
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,7 +7,7 @@ import "../../global.css";
 
 function About() {
 	const podcastLink =
-		"https://www.youtube.com/embed/videoseries?si=ZobjNMvDAoBIorPw&controls=0&list=PLvXySQVib-mmNoOeoORHRGz2UyeSEgj7Q&autoplay=0&loop=1&mute=0";
+		"https://www.youtube.com/embed/videoseries?si=ZobjNMvDAoBIorPw&controls=0&list=PLvXySQVib-mmNoOeoORHRGz2UyeSEgj7Q&autoplay=1&loop=1&mute=0";
 	const thumbnailLink = "https://img.youtube.com/vi/un8x-gcfQUw/hqdefault.jpg";
 
 	return (
