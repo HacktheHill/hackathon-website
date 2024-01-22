24eb9046c2f940b2766423a4e22cbd117bf1f68c
Daniel Thorp <arcanistzed@gmail.com>
2024-01-22 06:54:14 -0500

    fix: shifting of testimonials during transition
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -5,46 +5,46 @@ import styles from "./Testimonials.module.css";
 function Testimonials() {
 	const testimonialData = [
 		{
-			id: 1,
-			name: t("testimonials.t1.name"),
-			content: t("testimonials.t1.content"),
-			role: t("testimonials.t1.role"),
-			img: "https://media.licdn.com/dms/image/C5603AQFpgrXO0Lnvdw/profile-displayphoto-shrink_800_800/0/1575907293069?e=1710979200&v=beta&t=WASkaJhAytMkAv2IvasoS4N7wrf2hIjym7RfSofckJY",
-		},
-		{
-			id: 2,
-			name: t("testimonials.t2.name"),
-			content: t("testimonials.t2.content"),
-			role: t("testimonials.t2.role"),
-			img: "https://media.licdn.com/dms/image/D4E03AQFmj_vlNBKr9g/profile-displayphoto-shrink_800_800/0/1678194204883?e=1710979200&v=beta&t=-8_THT5DFd97CeXlJEluQo74uEr-Rjgq2XEi-gerw8k",
-		},
-		{
 			id: 3,
 			name: t("testimonials.t3.name"),
 			content: t("testimonials.t3.content"),
 			role: t("testimonials.t3.role"),
-			img: "https://media.licdn.com/dms/image/D4E03AQF5bPQP0O7QOw/profile-displayphoto-shrink_800_800/0/1683226303156?e=1710979200&v=beta&t=vnScv9JWtj90T32hAy2RCqJTloT1hzF9Pq_sMyiMZ1w",
-		},
-		{
-			id: 4,
-			name: t("testimonials.t4.name"),
-			content: t("testimonials.t4.content"),
-			role: t("testimonials.t4.role"),
-			img: "https://cdn.discordapp.com/attachments/1045803128869634088/1180289992568881182/nyah.png",
-		},
-		{
-			id: 5,
-			name: t("testimonials.t5.name"),
-			content: t("testimonials.t5.content"),
-			role: t("testimonials.t5.role"),
-			img: "https://media.licdn.com/dms/image/D4E03AQH_j8ydFFqCDQ/profile-displayphoto-shrink_800_800/0/1680969580815?e=1710979200&v=beta&t=vOXZS26XmwPPU2ZRG2n-JY-kYlemw9dhVLh80We2DA4",
+			img: "https://cdn1.hackthehill.com/testimonials/britt-hayman.webp",
 		},
 		{
 			id: 6,
 			name: t("testimonials.t6.name"),
 			content: t("testimonials.t6.content"),
 			role: t("testimonials.t6.role"),
-			img: "headshots/testimonials/greg.png",
+			img: "https://cdn1.hackthehill.com/testimonials/greg-suignard.webp",
+		},
+		{
+			id: 1,
+			name: t("testimonials.t1.name"),
+			content: t("testimonials.t1.content"),
+			role: t("testimonials.t1.role"),
+			img: "https://cdn1.hackthehill.com/testimonials/maddie-whibbs.webp",
+		},
+		{
+			id: 2,
+			name: t("testimonials.t2.name"),
+			content: t("testimonials.t2.content"),
+			role: t("testimonials.t2.role"),
+			img: "https://cdn1.hackthehill.com/testimonials/adam-laderoute.webp",
+		},
+		{
+			id: 4,
+			name: t("testimonials.t4.name"),
+			content: t("testimonials.t4.content"),
+			role: t("testimonials.t4.role"),
+			img: "https://cdn1.hackthehill.com/testimonials/nyah-wagner.webp",
+		},
+		{
+			id: 5,
+			name: t("testimonials.t5.name"),
+			content: t("testimonials.t5.content"),
+			role: t("testimonials.t5.role"),
+			img: "https://cdn1.hackthehill.com/testimonials/hasin-zaman.webp",
 		},
 	];
 
@@ -85,7 +85,6 @@ function Testimonials() {
 		return () => {
 			carousel.removeEventListener("touchstart", handleTouchStart, false);
 			carousel.removeEventListener("touchend", handleTouchEnd, false);
-			clearInterval(interval);
 		};
 	}, []);
 
@@ -95,37 +94,41 @@ function Testimonials() {
 			<h1>{t("testimonials.title")}</h1>
 			<h2>{t("testimonials.sub_heading")}</h2>
 			<div className={styles["testimonial-body"]} ref={ref}>
-				<img
-					className={styles["testimonial-img"]}
-					src={testimonialData[activeIndex].img}
-					alt={testimonialData[activeIndex].name}
-				/>
-				<div className={styles["testimonial-container"]}>
-					<div className={styles["testimonial-content-container"]}>
-						<p className={styles["testimonial-content"]}>{testimonialData[activeIndex].content}</p>
-						<p className={styles["testimonial-provider"]}>
-							{testimonialData[activeIndex].name}, {testimonialData[activeIndex].role}
-						</p>
-					</div>
-					<div className={styles["carousel-control"]}>
-						<button
-							onClick={prevSlide}
-							className={styles["prev-button"]}
-							aria-label="Previous Icon"
-						></button>
-						<div className={styles["carousel-dots"]}>
-							{testimonialData.map((_, index) => (
-								<span
-									key={index}
-									className={`${styles.dot} ${index === activeIndex ? styles.active : ""}`}
-									onClick={() => setActiveIndex(index)}
-									onKeyDown={() => setActiveIndex(index)}
-								></span>
-							))}
+				{testimonialData.map((_, index) => (
+					<div
+						key={index}
+						className={styles["testimonial-container"]}
+						style={{
+							transform: `translateX(calc(${-activeIndex * 100}% - ${activeIndex * 4}rem))`,
+						}}
+					>
+						<img
+							className={styles["testimonial-img"]}
+							src={testimonialData[index].img}
+							alt={testimonialData[index].name}
+						/>
+						<div className={styles["testimonial-text"]}>
+							<p className={styles["testimonial-content"]}>{testimonialData[index].content}</p>
+							<p className={styles["testimonial-provider"]}>
+								{testimonialData[index].name}, {testimonialData[index].role}
+							</p>
 						</div>
-						<button onClick={nextSlide} className={styles["next-button"]} aria-label="Next Icon"></button>
 					</div>
+				))}
+			</div>
+			<div className={styles["carousel-control"]}>
+				<button onClick={prevSlide} className={styles["prev-button"]}></button>
+				<div className={styles["carousel-dots"]}>
+					{testimonialData.map((_, index) => (
+						<button
+							key={index}
+							className={index === activeIndex ? styles.active : ""}
+							onClick={() => setActiveIndex(index)}
+							onKeyDown={() => setActiveIndex(index)}
+						></button>
+					))}
 				</div>
+				<button onClick={nextSlide} className={styles["next-button"]}></button>
 			</div>
 			<img className={styles["right-leaves"]} src="/SVGs/Testimonials/right-leaves.svg" alt="right-leaves" />
 		</div>
