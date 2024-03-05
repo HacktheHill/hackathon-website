f78a0e3effa1eed599d63cc98153b62159cfe58a
Stefan Todorovic <stefantodorovic@gmail.com>
2024-03-05 04:14:30 -0500

    fix: site map

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ const DEV = process.argv[2] === "build" ? false : true;
 // https://astro.build/config
 export default defineConfig({
 	outDir: "build",
-	site: DEV ? "http://localhost:3000" : "https://hackthehill.com",
+	site: DEV ? "http://localhost:3000" : "https://2023.hackthehill.com",
 	integrations: [react(), sitemap()],
 });
