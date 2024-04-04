9aefb1d13664cf2bda70839643d02899d3d86e68
Osa Ikhinmwin <osaikh@Osas-MacBook-Pro.local>
2024-04-04 19:42:39 -0400

    feat: pr changes

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,27 +1,30 @@
 import React, { useState } from "react";
 import { t } from "../../i18n";
+import hth1 from "/src/assets/gallery/panel1.webp"
 
 export default function About() {
-	const [activeImage, setActiveImage] = useState("src/assets/about/Rectangle_17.png");
+	const [activeImage, setActiveImage] = useState(0);
 
-	const getImageClassName = imageSrc => {
+	const getImageClassName = index => {
 		return `relative w-full h-full object-cover border-2 border-red-500 transition-all duration-300 hover:translate-x-[-5px] hover:translate-y-[-5px] hover:shadow-[4px_4px_0px_black] ${
-			activeImage === imageSrc
+			activeImage === index
 				? "z-10 bg-opacity-50 border-shade-3 bg-light_accent active:translate-x-[0px] active:translate-y-[0px] active:shadow-none"
-				: "z-0"
+				: "z-0 cursor-pointer"
 		}`;
 	};
 
 	return (
 		<>
-			<div className="w-full bg-shade-9 justify-center items-center bg-square-svg bg-center bg-cover bg-no-repeat bg-fixed ">
+			<div className="w-full bg-shade-9 justify-center items-center bg-flat-mountains-svg bg-center bg-cover bg-no-repeat bg-fixed ">
 				<div className="flex flex-col w-full h-full justify-center items-center gap-16 py-36 text-center">
+				<div className="flex flex-col px-8 gap-4">
+					<h1>It's more than just a hackathon</h1>
+					<h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, aliquid.
+					</h2>
+				</div>
 					<div className="flex w-9/12 h-4/6 flex-row justify-between items-center flex-wrap xl:gap-16 xl:w-11/12">
-						<div className="flex basis-6/12 justify-start items-center flex-wrap gap-8 xl:basis-full xl:justify-center">
+						<div className="flex basis-5/12 justify-start items-center flex-wrap gap-8 xl:basis-full xl:justify-center">
 							<div className=" text-left ">
-								<h2 className=" w-5/6 text-3xl mb-12 px-6 py-2 font-italic font-medium ">
-									It's more than just a hackathon
-								</h2>
 								<div className="w-6/7 text-pretty p-3 ">
 									<p className="">
 										Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
@@ -43,20 +46,20 @@ export default function About() {
 						>
 							<div className="border-2 col-start-1 col-end-9 row-start-9 row-end-13 aos-frame aos-animate">
 								<img
-									src="src/assets/about/Rectangle_15.png"
+									src="/src/assets/gallery/panel1.webp"
 									alt="Description"
-									className={getImageClassName("src/assets/about/Rectangle_15.png")}
-									onClick={() => setActiveImage("src/assets/about/Rectangle_15.png")}
+									className={getImageClassName(0)}
+									onClick={() => setActiveImage(0)}
 								/>
 							</div>
 							<div
 								className="border-2 col-start-1 col-end-6 row-start-1 row-end-6 xs:h-36 aos-frame aos-animate"
 							>
 								<img
-									src="src/assets/about/Rectangle_16.png"
+									src="/src/assets/gallery/hth1.webp"
 									alt="Description"
-									className={getImageClassName("src/assets/about/Rectangle_16.png")}
-									onClick={() => setActiveImage("src/assets/about/Rectangle_16.png")}
+									className={getImageClassName(1)}
+									onClick={() => setActiveImage(1)}
 								/>
 							</div>
 							<div
@@ -64,10 +67,10 @@ export default function About() {
 								data-aos="zoom-in"
 							>
 								<img
-									src="src/assets/about/Rectangle_17.png"
+									src="/src/assets/gallery/hth2.webp"
 									alt="Description"
-									className={getImageClassName("src/assets/about/Rectangle_17.png")}
-									onClick={() => setActiveImage("src/assets/about/Rectangle_17.png")}
+									className={getImageClassName(2)}
+									onClick={() => setActiveImage(2)}
 								/>
 							</div>
 						</div>
