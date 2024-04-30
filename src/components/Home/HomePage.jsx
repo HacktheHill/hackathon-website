3179e7e3b6cf35cf87bf04a5aad2498cf91bd18a
Sacha Arseneault <sacha.arseneault@gmail.com>
2024-04-30 18:31:59 -0400

    feat: testimonials

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -16,7 +16,7 @@ export default function HomePage() {
 			<Stats />
 			<Gallery />
 			<Sponsors />
-			{/*<Testimonials />*/}
+			<Testimonials />
 			{/*<Contact />*/}
 		</>
 	);
--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
 import { t } from "../../i18n";
-import styles from "./Testimonials.module.css";
 
 export default function Testimonials() {
 	const testimonialData = [
@@ -38,13 +37,6 @@ export default function Testimonials() {
 			content: t("testimonials.t2.content"),
 			role: t("testimonials.t2.role"),
 			img: "https://cdn1.hackthehill.com/testimonials/adam-laderoute.webp",
-		},
-		{
-			id: 4,
-			name: t("testimonials.t4.name"),
-			content: t("testimonials.t4.content"),
-			role: t("testimonials.t4.role"),
-			img: "https://cdn1.hackthehill.com/testimonials/nyah-wagner.webp",
 		},
 	];
 
@@ -89,57 +81,64 @@ export default function Testimonials() {
 	}, []);
 
 	return (
-		<div id="testimonials" className={styles["testimonials"]}>
-			<h1>{t("testimonials.title")}</h1>
-			<h2>{t("testimonials.sub_heading")}</h2>
-			<div className={styles["testimonial-body"]} ref={ref}>
-				{testimonialData.map((_, index) => (
-					<div
-						key={index}
-						className={styles["testimonial-container"]}
-						style={{
-							transform: `translateX(calc(${-activeIndex * 100}% - ${activeIndex * 4}rem))`,
-						}}
-					>
-						<img
-							className={styles["testimonial-img"]}
-							src={testimonialData[index].img}
-							alt={testimonialData[index].name}
-						/>
-						<div className={styles["testimonial-text"]}>
-							<p className={styles["testimonial-content"]}>{testimonialData[index].content}</p>
-							<p className={styles["testimonial-provider"]}>
-								{testimonialData[index].name}, {testimonialData[index].role}
-							</p>
-						</div>
-					</div>
-				))}
-			</div>
-			<div className={styles["carousel-control"]}>
-				<button
-					onClick={prevSlide}
-					className={styles["prev-button"]}
-					aria-label={t("testimonials.aria_label_prev")}
-				></button>
-				<div className={styles["carousel-dots"]}>
-					{testimonialData.map((_, index) => (
-						<button
-							key={index}
-							className={index === activeIndex ? styles.active : ""}
-							onClick={() => setActiveIndex(index)}
-							onKeyDown={() => setActiveIndex(index)}
-							aria-label={t("testimonials.aria_label_dot")}
-						></button>
-					))}
+		<div className="w-full bg-shade-9 justify-center items-center bg-pentagon-svg bg-center bg-cover bg-no-repeat">
+			<div className="flex flex-col w-full h-full justify-center items-center gap-20 py-36 text-center lg:gap-8">
+				<div className="flex flex-col w-[70%] px-8 gap-4 md:w-full">
+					<h1>{t("testimonials.title")}</h1>
+					<h2>{t("testimonials.sub_heading")}</h2>
 				</div>
-				<button
-					onClick={nextSlide}
-					className={styles["next-button"]}
-					aria-label={t("testimonials.aria_label_next")}
-				></button>
-				<button onClick={nextSlide} className="bg-primary font-bold text-white px-4 py-2 rounded-md">
-					yo
-				</button>
+				<div
+					ref={ref}
+					className="flex w-9/12 h-4/6 flex-row justify-center items-center flex-wrap xl:gap-16 xl:w-11/12"
+				>
+					<div className="grid grid-flow-col auto-cols-[100%] w-[70%] gap-16 overflow-hidden md:w-[90%]">
+						{testimonialData.map((_, index) => (
+							<div
+								key={index}
+								className="flex flex-row items-center justify-center gap-[5vw] lg:flex lg:flex-wrap lg:items-start lg:gap-8"
+								style={{
+									transform: `translateX(calc(${-activeIndex * 100}% - ${activeIndex * 4}rem))`,
+								}}
+							>
+								<img
+									className="box-border h-64 w-64 aspect-square object-cover rounded-[50%] p-4 border-4 border-primary bg-light_accent bg-opacity-50 md:h-48 md:w-48"
+									src={testimonialData[index].img}
+									alt={testimonialData[index].name}
+								/>
+								<div className="basis-2/3 flex flex-col justify-center gap-4 text-left border-2 bg-opacity-100 border-shade-3 rounded-sm bg-shade-8 z-20 px-8 py-12">
+									<p className="">{testimonialData[index].content}</p>
+									<p className="font-bold">
+										{testimonialData[index].name}, {testimonialData[index].role}
+									</p>
+								</div>
+							</div>
+						))}
+					</div>
+				</div>
+				<div className="flex gap-8">
+					<button
+						onClick={prevSlide}
+						className="border-r-[0.375rem] border-b-[0.375rem] border-primary rounded-[20%] inline-block p-[0.375rem] bg-none cursor-pointer transition-all duration-300 rotate-[135deg] opacity-80 hover:opacity-100"
+						aria-label={t("testimonials.aria_label_prev")}
+					></button>
+					<div className="flex justify-evenly items-center gap-3">
+						{testimonialData.map((_, index) => (
+							<button
+								key={index}
+								className={`block h-4 w-4 border-none bg-light_accent rounded-md cursor-pointer transition-all duration-300 hover:opacity-100
+								${index === activeIndex ? "opacity-100" : "opacity-50"}`}
+								onClick={() => setActiveIndex(index)}
+								onKeyDown={() => setActiveIndex(index)}
+								aria-label={t("testimonials.aria_label_dot")}
+							></button>
+						))}
+					</div>
+					<button
+						onClick={nextSlide}
+						className="border-r-[0.375rem] border-b-[0.375rem] border-primary rounded-[20%] inline-block p-[0.375rem] bg-none cursor-pointer transition-all duration-300 rotate-[-45deg] opacity-80 hover:opacity-100"
+						aria-label={t("testimonials.aria_label_next")}
+					></button>
+				</div>
 			</div>
 		</div>
 	);
