511032d67487efec60a726194964fbc64601c930
Sacha Arseneault <sacha.arseneault@gmail.com>
2024-05-02 21:42:51 -0400

    feat: new sponsors

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -31,7 +31,7 @@ export default function Hero() {
 					<img id="hth-logo-outline" {...HtHoutline} alt="Hack the Hill Logo" className="absolute w-3/4" />
 					<img id="hth-logo-solid" {...HtHSolid} alt="Hack the Hill Logo" className="relative w-3/4" />
 				</div>
-				<div id="content" className="flex flex-col w-full justify-center items-center gap-8 px-4 md:gap-0">
+				<div id="content" className="flex flex-col w-full justify-center items-center gap-8 px-4 md:gap-4">
 					<h2 className="font-medium text-3xl text-center md:text-lg">{t("hero.title")}</h2>
 					<Button onClick={() => window.open("https://2024.hackthehill.com", "_blank")}>2024</Button>
 				</div>
--- src/components/Sponsors/Sponsors.jsx
+++ src/components/Sponsors/Sponsors.jsx
@@ -13,78 +13,83 @@ import echo3d from "/src/assets/Logos/Partners/Echo3d.svg";
 import voiceflow from "/src/assets/Logos/Partners/Voiceflow.svg";
 import StickerMule from "/src/assets/Logos/Partners/StickerMule.svg";
 import OpenProject from "/src/assets/Logos/Partners/OpenProject.svg";
+import "./Sponsors.css";
 
 export default function Sponsors() {
 	const data = {
 		sponsors: [
 			{ href: "https://ciena.ca/", ...Ciena, alt: "Ciena" },
 			{ href: "https://blackberry.com/", ...Blackberry, alt: "Blackberry" },
-			{
-				href: "https://canadiantire.ca/",
-				...CanadianTire,
-				alt: "Canadian Tire",
-			},
+			{ href: "https://canadiantire.ca/", ...CanadianTire, alt: "Canadian Tire" },
 			{ href: "https://lonehaven.com/", ...lonehaven, alt: "Lonehaven" },
 			{ href: "https://www.cse-cst.gc.ca/", ...CSE, alt: "CSE / CST" },
-
-			{ href: "https://vercel.com/", ...Vercel, alt: "Vercel", darkBg: false, needsShadow: false },
-			{
-				href: "https://www.digitalocean.com/",
-				...DigitalOcean,
-				alt: "DigitalOcean",
-				darkBg: true,
-				needsShadow: false,
-			},
-			{ href: "https://www.echo3d.com/", ...echo3d, alt: "echo3D", darkBg: false, needsShadow: false },
-			{ href: "https://balsamiq.com/", ...balsamiq, alt: "balsamiq", darkBg: true, needsShadow: false },
-			{ href: "https://www.voiceflow.com/", ...voiceflow, alt: "Voiceflow", darkBg: false, needsShadow: false },
-			{ href: "https://about.google", ...Google, alt: "Google", darkBg: true, needsShadow: false },
-
-			{
-				href: "https://www.openproject.org/",
-				...OpenProject,
-				alt: "OpenProject",
-				darkBg: false,
-				needsShadow: false,
-			},
-			{ href: "https://mule.to/p5ni", ...StickerMule, alt: "StickerMule", darkBg: true, needsShadow: true },
+			{ href: "https://vercel.com/", ...Vercel, alt: "Vercel" },
+			{ href: "https://www.digitalocean.com/", ...DigitalOcean, alt: "DigitalOcean" },
+			{ href: "https://www.echo3d.com/", ...echo3d, alt: "echo3D" },
+			{ href: "https://balsamiq.com/", ...balsamiq, alt: "balsamiq" },
+			{ href: "https://www.voiceflow.com/", ...voiceflow, alt: "Voiceflow" },
+			{ href: "https://about.google", ...Google, alt: "Google" },
+			{ href: "https://www.openproject.org/", ...OpenProject, alt: "OpenProject" },
+			{ href: "https://mule.to/p5ni", ...StickerMule, alt: "StickerMule" },
 		],
 	};
 
 	const [hovered, setHovered] = useState(-1);
 
+	const marqueeGroup = (data, index, pauseAnimation, startAnimation) => {
+		return (
+			<div
+				id={`marquee${index}`}
+				className="marquee-group"
+				onMouseEnter={pauseAnimation}
+				onMouseLeave={startAnimation}
+				onFocus={pauseAnimation}
+				onBlur={startAnimation}
+			>
+				{data.sponsors.map((sponsor, i) => (
+					<a
+						key={i}
+						href={sponsor.href}
+						target="_blank"
+						rel="noreferrer"
+						className={`sponsor flex aspect-square justify-center items-center rounded-lg h-52 md:h-36 p-8 md:p-4 transition-all duration-200
+					 ${
+							hovered !== -1 && hovered !== i
+								? "opacity-25 translate-x-1 translate-y-1"
+								: "bg-opacity-35 translate-x-0 translate-y-0"
+						}`}
+						onMouseEnter={() => setHovered(i)}
+						onMouseLeave={() => setHovered(-1)}
+						onBlur={() => setHovered(-1)}
+					>
+						<img {...sponsor} alt={`${sponsor.alt} logo`} className="max-w-full max-h-full"></img>
+					</a>
+				))}
+			</div>
+		);
+	};
+
+	function pauseAnimation() {
+		document.getElementById("marquee1").style.animationPlayState = "paused";
+		document.getElementById("marquee2").style.animationPlayState = "paused";
+	}
+
+	function startAnimation() {
+		document.getElementById("marquee1").style.animationPlayState = "running";
+		document.getElementById("marquee2").style.animationPlayState = "running";
+	}
+
 	return (
 		<>
 			<div className="w-full flex justify-center items-center">
-				<div className="flex flex-col w-full h-full justify-center items-center gap-16 py-36 text-center max-w-2xl">
+				<div className="flex flex-col w-full h-full justify-center items-center gap-16 text-center max-w-2xl">
 					<div className="flex flex-col px-8 gap-4">
 						<h1>{t("sponsors.title")}</h1>
 						<h2>{t("sponsors.subtitle")}</h2>
 					</div>
-					<div className="grid grid-cols-3 gap-12 w-9/12 lg:grid-cols-2 lg:gap-8">
-						{data.sponsors.map((sponsor, i) => (
-							<a
-								key={i}
-								href={sponsor.href}
-								target="_blank"
-								rel="noreferrer"
-								className={`flex justify-center items-center rounded-lg h-52 md:h-32 p-8 md:p-4 transition-all duration-200
-								 ${
-										hovered !== -1 && hovered !== i
-											? "opacity-25 translate-x-1 translate-y-1"
-											: "bg-opacity-35 translate-x-0 translate-y-0"
-									}`}
-								onMouseEnter={() => setHovered(i)}
-								onMouseLeave={() => setHovered(-1)}
-								onBlur={() => setHovered(-1)}
-							>
-								<img
-									alt={`${sponsor.alt} logo`}
-									src={sponsor.src}
-									className="max-w-full max-h-full"
-								></img>
-							</a>
-						))}
+					<div className="carousel-track">
+						{marqueeGroup(data, 1, pauseAnimation, startAnimation)}
+						{marqueeGroup(data, 2, pauseAnimation, startAnimation)}
 					</div>
 				</div>
 			</div>
