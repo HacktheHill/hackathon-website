4a3148b626ad464f490402ba01aa63dfb931d1f3
Sacha Arseneault <sacha.arseneault@gmail.com>
2024-05-09 21:36:57 -0400

    feat: beaver4

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { t } from "../../i18n";
 import quote from "../../assets/icons/quote.svg";
 import Button from "../Button/Button";
+import beaver4 from "../../assets/Beaver4.svg";
 
 export default function Testimonials() {
 	const testimonialData = [
@@ -51,7 +52,31 @@ export default function Testimonials() {
 				</div>
 				<div className="grid gap-4 grid-rows-12 grid-cols-12 w-full max-w-2xl xl:flex xl:flex-col xl:px-4 xl:gap-2">
 					<div
-						className="flex flex-col justify-between items-start gap-8 bg-blur-svg overflow-hidden rounded-3xl p-8 row-start-1 col-start-1 row-end-8 col-end-8"
+						className="flex flex-col justify-between items-start gap-8 bg-blur-svg rounded-3xl p-8 row-start-1 col-start-8 row-end-10 col-end-13"
+						data-aos="fade-left"
+					>
+						<div className="flex flex-row items-center gap-4">
+							<img
+								className="box-border h-20 w-20 aspect-square object-cover rounded-[50%]"
+								src={testimonialData[2].img}
+								alt={testimonialData[2].name}
+							/>
+							<div>
+								<h4>{testimonialData[2].name}</h4>
+								<p>{testimonialData[2].role}</p>
+							</div>
+						</div>
+						<div className="flex flex-row gap-6">
+							<img {...quote} alt="quote" className="w-6 aspect-square self-start opacity-85" />
+							<p>{testimonialData[2].content}</p>
+							<img {...quote} alt="quote" className="-scale-100 w-6 aspect-square self-end opacity-85" />
+						</div>
+						<div className="absolute -top-16 right-0 h-24">
+							<img src={beaver4.src} alt="Beaver" className="h-full" />
+						</div>
+					</div>
+					<div
+						className="flex flex-col justify-between items-start gap-8 bg-[#020106] overflow-hidden rounded-3xl p-8 row-start-1 col-start-1 row-end-8 col-end-8"
 						data-aos="fade-right"
 					>
 						<div className="flex flex-row items-center gap-4">
@@ -72,12 +97,12 @@ export default function Testimonials() {
 						</div>
 					</div>
 					<div
-						className="flex flex-col justify-between items-start gap-8 bg-[#020106] overflow-hidden rounded-3xl p-8 row-start-8 col-start-1 row-end-13 col-end-8"
+						className="flex flex-col justify-between items-start gap-8 bg-blur-svg overflow-hidden rounded-3xl p-8 row-start-8 col-start-1 row-end-13 col-end-8"
 						data-aos="fade-up"
 					>
 						<div className="flex flex-row items-center gap-4">
 							<img
-								className="box-border h-20 w-20 aspect-square object-cover rounded-[50%] shadow-glow"
+								className="box-border h-20 w-20 aspect-square object-cover rounded-[50%]"
 								src={testimonialData[1].img}
 								alt={testimonialData[1].name}
 							/>
@@ -93,28 +118,7 @@ export default function Testimonials() {
 						</div>
 					</div>
 					<div
-						className="flex flex-col justify-between items-start gap-8 bg-[#020106] overflow-hidden rounded-3xl p-8 row-start-1 col-start-8 row-end-10 col-end-13"
-						data-aos="fade-left"
-					>
-						<div className="flex flex-row items-center gap-4">
-							<img
-								className="box-border h-20 w-20 aspect-square object-cover rounded-[50%] shadow-glow"
-								src={testimonialData[2].img}
-								alt={testimonialData[2].name}
-							/>
-							<div>
-								<h4>{testimonialData[2].name}</h4>
-								<p>{testimonialData[2].role}</p>
-							</div>
-						</div>
-						<div className="flex flex-row gap-6">
-							<img {...quote} alt="quote" className="w-6 aspect-square self-start opacity-85" />
-							<p>{testimonialData[2].content}</p>
-							<img {...quote} alt="quote" className="-scale-100 w-6 aspect-square self-end opacity-85" />
-						</div>
-					</div>
-					<div
-						className="flex flex-col justify-center items-between text-end bg-blur-svg rounded-3xl p-8 row-start-10 col-start-8 row-end-13 col-end-13"
+						className="flex flex-col justify-center items-between text-end bg-[#020106] rounded-3xl p-8 row-start-10 col-start-8 row-end-13 col-end-13"
 						data-aos="fade-up"
 					>
 						<div className="self-end">
