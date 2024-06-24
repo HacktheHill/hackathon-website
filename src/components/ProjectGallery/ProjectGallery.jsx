a5a7ecef24ec0d10dc379de5ac0039b9cf374f59
Osa <eruikhinmwin@gmail.com>
2024-06-24 12:48:50 -0400

    feat: animations

--- src/components/ProjectGallery/ProjectGallery.jsx
+++ src/components/ProjectGallery/ProjectGallery.jsx
@@ -1,61 +1,73 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./ProjectGallery.module.css";
+import Button from "../Button/Button.jsx";
+
+// animations
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const projects = [
-  {
-    id: "infracrypt",
-    href: "https://devpost.com/software/infracrypt",
-    imgSrc: "https://via.placeholder.com/400x300",
-    altText: "infracrypt Banner",
-    delay: 200
-  },
-  {
-    id: "oiligarchy",
-    href: "https://devpost.com/software/oiligarchy-the-game-about-how-profit-turns-to-pain",
-    imgSrc: "https://via.placeholder.com/400x300",
-    altText: "oiligarchy Banner",
-    delay: 300
-  },
-  {
-    id: "onionalyze",
-    href: "https://devpost.com/software/onionalyze",
-    imgSrc: "https://via.placeholder.com/400x300",
-    altText: "onionalyze Banner",
-    delay: 400
-  },
+	{
+		id: "ColourSense",
+		href: "https://devpost.com/software/colour-sense",
+		imgSrc: "https://d112y698adiu2z.cloudfront.net/photos/production/software_photos/002/408/913/datas/gallery.jpg",
+		altText: "ColourSense Banner",
+		delay: 200,
+	},
+	{
+		id: "oiligarchy",
+		href: "https://devpost.com/software/oiligarchy-the-game-about-how-profit-turns-to-pain",
+		imgSrc: "https://d112y698adiu2z.cloudfront.net/photos/production/software_photos/002/408/722/datas/original.png",
+		altText: "oiligarchy Banner",
+		delay: 300,
+	},
+	{
+		id: "onionalyze",
+		href: "https://devpost.com/software/onionalyze",
+		imgSrc: "https://d112y698adiu2z.cloudfront.net/photos/production/software_photos/002/407/402/datas/gallery.jpg",
+		altText: "onionalyze Banner",
+		delay: 400,
+	},
 ];
 
 function ProjectGallery() {
-  console.log("ProjectGallery rendered");
-  return (
-    <section id="gallery" className={styles.gallery}>
-      <div className={styles.container}>
-        <div className={styles.heading}>
-          <p className={styles.title}>&lt;project gallery /&gt;</p>
-          <h3 className={styles.subtitle}>last year's top 3&nbsp;🏆</h3>
-        </div>
-        <div className={styles.projects}>
-          {projects.map((project, index) => (
-            <div
-              key={project.id}
-              className={`${styles.project} ${index === 1 ? styles.middleProject : ""}`}
-            >
-              <a href={project.href}>
-                <img
-                  className={styles.projectImage}
-                  src={project.imgSrc}
-                  alt={project.altText}
-                />
-              </a>
-            </div>
-          ))}
-        </div>
-        <p className={styles.moreInspiration}>
-          <a href="https://hack-the-hill.devpost.com/project-gallery">need more inspiration?</a>
-        </p>
-      </div>
-    </section>
-  );
+	useEffect(() => {
+		AOS.init();
+	}, []);
+
+	console.log("ProjectGallery rendered");
+	return (
+		<section id="gallery" className={styles.gallery}>
+			<div className={styles.container}>
+				<div className={styles.heading}>
+					<p className={styles.subtitle}>Highlighting Last Year's Top Achievements</p>
+				</div>
+				<div className={styles.projects}>
+					{projects.map((project, index) => (
+						<div
+							key={project.id}
+							className={`${styles.project} ${index === 1 ? styles.middleProject : ""}`}
+							data-aos="fade-up"
+							data-aos-delay={project.delay}
+						>
+							<a href={project.href}>
+								<img className={styles.projectImage} src={project.imgSrc} alt={project.altText} />
+							</a>
+						</div>
+					))}
+				</div>
+				<p className={styles.moreInspiration}>
+					<Button
+						href="https://hack-the-hill.devpost.com/project-gallery"
+						data-aos="fade-up"
+						data-aos-duration="800"
+					>
+						Explore more innovative projects
+					</Button>
+				</p>
+			</div>
+		</section>
+	);
 }
 
 export default ProjectGallery;
