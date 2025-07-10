2a8ce69271f895fd90a211d0502970582689f22c
Jonathan Eveillard <Jonathaneveillard@cmail.carleton.ca>
2025-07-10 19:28:39 -0400

    style: navigation bar changed to current 2025 design

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -12,10 +12,10 @@ export default {
 		aria_label: "Navigation Bar",
 	},
 	hero: {
-		format: "In person",
+		format: "",
 		at: "@",
 		pin_alt: "Location Pin",
-		h2: "Canada's Capital Hackathon",
+		h2: "",
 		h3: "Ottawa's Largest Hackathon",
 		more: "Follow us!",
 		link: "https://www.youtube.com/watch?v=MXwlAdZL3t0&ab_channel=Lonehaven",
@@ -23,7 +23,7 @@ export default {
 		invalid_email_field: "Invalid email format",
 		email_placeholder: "E-mail",
 		apply: "Apply Now",
-		date: "Sept. 27-29, 2024",
+		date: "TBD",
 	},
 	about: {
 		title: "Welcome to Canada's Capital Hackathon",
