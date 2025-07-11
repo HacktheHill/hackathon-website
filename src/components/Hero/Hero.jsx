65040f777772d1114982030de83add10fc277e3f
jonathaneveillard <jonathaneveillard@cmail.carleton.ca>
2025-07-11 14:40:13 -0400

    feature: Local Set Up

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import styles from './Hero.module.css'
+import {useEffect, useRef, useState} from 'react';
+import styles from './Hero.module.css';
+import "./animations.css";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon as Icon } from "@fortawesome/react-fontawesome";
+import hero from "../../assets/hero.svg?raw";
+
 
 // Implementation of the Hero component
 const Hero = () => {
   return (
-    <div className={styles.container}>Hero</div>
+    <hero/>
   )
 }
 
